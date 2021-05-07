03ceb108d537724cfb6ebbb4fdf764cebf612cb2
Jony <jonyreiscardoso@gmail.com>
2021-05-07 14:41:14 -0400

    chore: add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Kassandra</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Kassandra</h1>
+      </main>
+    </div>
+  );
+}
