4b886b8130c14434a59c81d4da25d5ee8cb3c13a
Jony <jonyreiscardoso@gmail.com>
2021-05-10 15:50:56 -0400

    chore: add SASS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
