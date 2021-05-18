4e608d3576223ae479dd225582386067b20cd76a
Jony Reis <jonyreiscardoso@gmail.com>
2021-05-17 23:46:43 -0400

    feat: add Kassandra Architecture section on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,6 +63,21 @@ const Home = () => (
           </div>
         </div>
       </section>
+      <section className={styles['kassandra-architecture']}>
+        <h1>How it works?</h1>
+        <p>Our tokenized investment baskets are Automated Market Maker pools for multiple assets, with dynamically adjustable weights</p>
+        <img className={styles['img-kassandra-architecture']} src="assets/kassandra-architecture.svg" alt="" />
+        <div className={styles['grid-paragraph']}>
+          <div>
+            <p>A Smart Indexed Pool works essentially as an Automated Market Maker (AMM) for multiple assets, but with dynamically adjustable weights, that can be changed using external data.</p>
+            <p>This approach removes the need for rebalancing operations by creating small arbitrage opportunities that external users can profit from and ensure that the desired proportions of each asset in the investment basket are maintained.</p>
+          </div>
+          <div>
+            <p>By making a monetary deposit in a SIP, users receives a deposit representative token, namely Index Token, that represents a fractional ownership of assets in the pool. </p>
+            <p>The assets managed by the pool will have adjusted participations changed by the data provider, and at any time, the user can return his Index Token to the protocol to redeem his investment, which will be evaluated by the total value of the assets managed by the pool at the time of the redemption - working similar to a traditional market ETF.</p>
+          </div>
+        </div>
+      </section>
     </main>
   </div>
   )
