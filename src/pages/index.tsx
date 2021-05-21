73a4b087fad11db03572b72f5bea8536b11e8e4f
Jony Reis <jonyreiscardoso@gmail.com>
2021-05-21 16:04:23 -0400

    feat: add class-produts on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,27 @@ const Home = () => (
         <img className={styles['more-icon']} src="assets/more-icon.svg" alt="" />
         <p className={styles['more-text']}>More coming soon</p>
       </section>
+      <section className={styles['class-products']}>
+        <h1>A new class of investment products in DeFi</h1>
+        <p>Monetary efficient and actively managed decentralized investment baskets</p>
+        <ul>
+          <li>
+            <img src="assets/Icon/permissionless.svg" alt="Permissionless" />
+            <h3>Permissionless</h3>
+            <p>Invest, transfer and redeem investment products without relying on third-parties</p>
+          </li>
+          <li>
+            <img src="assets/Icon/non-custodial.svg" alt="Non-custodial" />
+            <h3>Non-custodial</h3>
+            <p>Your funds managed by public, secure, and predictable smart-contracts</p>
+          </li>
+          <li>
+            <img src="assets/Icon/actively-managed.svg" alt="Actively managed" />
+            <h3>Actively managed</h3>
+            <p>Outsource the management of your money to data-driven quantitative models</p>
+          </li>
+        </ul>
+      </section>
       <section className={styles.token}>
         <h1>Be part of the Kassandra ecosystem with $KACY </h1>
         <p>Take the lead and join the first decentralized fund manager through our decentralized governance protocol</p>
