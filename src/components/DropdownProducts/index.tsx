cacd8a29fa31381b42b43c26340a6cd2fd076db2
Jony <jonyreiscardoso@gmail.com>
2021-05-30 18:23:24 -0400

    feat: add DropdownProducts component

---
+++ src/components/DropdownProducts/index.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+import styles from './dropdown.module.scss'
+
+const DropdownProducts = () => {
+  const [showProducts, setShowProducts] = React.useState(false);
+  const [positionBtnProducts, setPositionBtnProducts] = React.useState({})
+
+  function handleProductsPosition(e) {
+    const position = e.target.getClientRects()
+    setPositionBtnProducts({
+        x: position[0].x,
+        y: position[0].y + 20,
+    })
+  }
+
+  return (
+    <>
+      <div className={styles['btn-products']} onClick={(e) => {
+          setShowProducts(!showProducts)
+          handleProductsPosition(e)
+        }
+      }>
+        Products<img src="assets/arrow-down.svg" alt=""/>
+      </div>
+      {showProducts &&
+      <div className={styles['backdrop']}  onClick={() => setShowProducts(false)}>
+        <ul 
+          className={styles['products-container']} 
+          style={{
+            position: 'absolute', 
+            left: `${positionBtnProducts['x']}px`, 
+            top: `${positionBtnProducts['y']}px`
+          }}
+        >
+          <li>
+            <a href="products/heim">Heim Index</a>
+          </li>
+        </ul>
+      </div>
+      }
+    </>
+  )
+}
+
+export default DropdownProducts
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import Link from 'next/link'
+import DropdownProducts from '../DropdownProducts'
 
 import styles from './header.module.scss'
 
-const Header = () => (
-  <header className={styles.header}>
-    <img src="assets/logo-header.svg" alt="" className={styles['logo-header']} />
-    <img src="assets/logo-64.svg" alt="" className={styles['logo-64']} />
-    {/* <ul>
-      <li><Link href="products" >Products</Link></li>
-      <li><Link href="farm" >Farm</Link></li>
-      <li><Link href="vote" >Vote</Link></li>
-      <li><Link href="about" >About</Link></li>
-    </ul> */}
-    <ul>
-      <li><button type="button">Products</button></li>
-      <li><button type="button">Farm</button></li>
-      <li><button type="button">Vote</button></li>
-      <li><button type="button">About</button></li>
-    </ul>
-  </header>
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src="assets/logo-header.svg" alt="" className={styles['logo-header']} />
+      <img src="assets/logo-64.svg" alt="" className={styles['logo-64']} />
+      <nav>
+        <DropdownProducts />
+        <a href="farm" >Farm</a>
+        <a href="vote" >Vote</a>
+        <a href="about" >About</a>
+      </nav>
+      {/* <ul>
+        <li><button type="button">Products</button></li>
+        <li><button type="button">Farm</button></li>
+        <li><button type="button">Vote</button></li>
+        <li><button type="button">About</button></li>
+      </ul> */}
+    </header>
   )
+} 
+
 
 export default Header
