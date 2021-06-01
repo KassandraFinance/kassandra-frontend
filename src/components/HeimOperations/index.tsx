0ca2da24e3667b8865a085776dbd3dc7520223e2
Jony <jonyreiscardoso@gmail.com>
2021-05-31 23:04:39 -0400

    feat: add HeimOperations component on Products page

---
+++ src/components/HeimOperations/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Buy from './Buy'
+import styles from './heim-operations.module.scss'
+
+const HeimOperations = () => {
+  const [inputChecked, setInputChecked] = React.useState<string>('buy')
+  return (
+    <div className={styles['heim-operations']}>
+      <div className={styles['select-operator']}>
+        <input 
+          type="radio" 
+          name="operator" 
+          id="buy" 
+          onChange={() => setInputChecked('buy')}
+          checked={inputChecked === 'buy'}
+        />
+        <label htmlFor="buy">buy</label>
+        <input 
+          type="radio" 
+          name="operator" 
+          id="sell" 
+          onChange={() => setInputChecked('sell')}
+          checked={inputChecked === 'sell'}
+        />
+        <label htmlFor="sell">sell</label>
+        <input 
+          type="radio" 
+          name="operator" 
+          id="swap"
+          onChange={() => setInputChecked('swap')}
+          checked={inputChecked === 'swap'}
+        />
+        <label htmlFor="swap">swap</label>
+        <input 
+          type="radio" 
+          name="operator" 
+          id="mint"
+          onChange={() => setInputChecked('mint')}
+          checked={inputChecked === 'mint'}
+        />
+        <label htmlFor="mint">mint</label>
+        <input 
+          type="radio" 
+          name="operator" 
+          id="redeem"
+          onChange={() => setInputChecked('redeem')}
+          checked={inputChecked === 'redeem'}
+        />
+        <label htmlFor="redeem">redeem</label>
+      </div>
+      {inputChecked === 'buy' && <Buy />}
+    </div>
+  )
+}
+
+export default HeimOperations
