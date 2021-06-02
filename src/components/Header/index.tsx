3889dcfedcf28420df9451e5ff4ba6fbb8d43d42
Jony <jonyreiscardoso@gmail.com>
2021-06-02 16:53:57 -0400

    style: disabled links on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import DropdownProducts from '../DropdownProducts'
+// import DropdownProducts from '../DropdownProducts'
 
 import styles from './header.module.scss'
 
-const Header = () => {
-  return (
+const Header = () => (
     <header className={styles.header}>
       <Link href="/">
         <a><img src="assets/logo-header.svg" alt="" className={styles['logo-header']} /></a>
@@ -13,21 +12,20 @@ const Header = () => {
       <Link href="/">
         <a><img src="assets/logo-64.svg" alt="" className={styles['logo-64']} /></a>
       </Link>
-      <nav>
+      {/* <nav>
         <DropdownProducts />
         <Link href="/farm"><a>Stake/Farm</a></Link>
         <Link href="/vote"><a>Vote</a></Link>
         <Link href="/about"><a>About</a></Link>
-      </nav>
-      {/* <ul>
+      </nav> */}
+      <ul>
         <li><button type="button">Products</button></li>
         <li><button type="button">Farm</button></li>
         <li><button type="button">Vote</button></li>
         <li><button type="button">About</button></li>
-      </ul> */}
+      </ul>
     </header>
-  )
-} 
+  ) 
 
 
 export default Header
