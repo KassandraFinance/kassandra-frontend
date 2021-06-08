2853275ea00957071f8ffaa1a071b418f9096a79
Jony <jonyreiscardoso@gmail.com>
2021-06-08 01:28:35 -0400

    style: add responsive style on InputEth component

--- src/components/InputEth/index.tsx
+++ src/components/InputEth/index.tsx
@@ -1,26 +1,25 @@
+import React from 'react'
 import styles from './input-eth.module.scss'
 
 interface IInputEthProps {
   action: string
 }
 
-const InputEth = (props: IInputEthProps) => {
-  return(
-    <div className={styles['input-eth']}>
-      <div className={styles['pay-with']}>
-        <span>{props.action}</span>
-        <p>ETH</p>
-        <div className={styles['line']} />
-      </div>
-      <img className={styles['arrow-long']} src="assets/arrow-long-down.svg" alt="" />
-      <div className={styles['amount']}>
-        <span>Amount</span>
-        <input type="number" placeholder="0" />
-        <span>U$ 10.00</span>
-        <div className={styles['line']} />
-      </div>
+const InputEth = ({ action }: IInputEthProps) => (
+  <div className={styles['input-eth']}>
+    <div className={styles['pay-with']}>
+      <span>{action}</span>
+      <p>ETH</p>
+      <div className={styles.line} />
     </div>
-  )
-}
+    <img className={styles['arrow-long']} src="assets/arrow-long-down.svg" alt="" />
+    <div className={styles.amount}>
+      <span>Amount</span>
+      <input type="number" placeholder="0" />
+      <span>U$ 10.00</span>
+      <div className={styles.line} />
+    </div>
+  </div>
+)
 
 export default InputEth
