1e3378d2cad17a4aaf0a35b187141f8778db48d6
Jony <jonyreiscardoso@gmail.com>
2021-06-08 01:29:43 -0400

    style: add responsive style on InputHeim component

--- src/components/InputHeim/index.tsx
+++ src/components/InputHeim/index.tsx
@@ -7,18 +7,18 @@ interface IInputHeimProps {
 }
 
 const InputHeim = ({ action, redeem }: IInputHeimProps) => (
-    <div className={styles['input-heim']}>
-      <div className={styles['pay-with']}>
-        <span>{action}</span>
-        <p>ETH</p>
-      </div>
-      <img className={styles['arrow-long']} src={`assets/arrow-long-${redeem ? 'down' : 'up'}.svg`} alt="" />
-      <div className={styles.total}>
-        <span>Total</span>
-        <input type="number" placeholder="0" />
-        <span>U$ 10.00</span>
-      </div>
+  <div className={styles['input-heim']}>
+    <div className={styles['pay-with']}>
+      <span>{action}</span>
+      <p>ETH</p>
     </div>
-  )
+    <img className={styles['arrow-long']} src={`assets/arrow-long-${redeem ? 'down' : 'up'}.svg`} alt="" />
+    <div className={styles.total}>
+      <span>Total</span>
+      <input type="number" placeholder="0" />
+      <span>U$ 10.00</span>
+    </div>
+  </div>
+)
 
 export default InputHeim
