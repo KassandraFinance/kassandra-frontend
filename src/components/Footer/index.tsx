6ee6e5f0c00c4b91409506b93094c8ed23ccc1c9
Jony <jonyreiscardoso@gmail.com>
2021-06-08 01:21:40 -0400

    style: update responsive style on Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -55,6 +55,58 @@ const Footer = () => (
           </ul>
         </div>
       </div>
+      <div className={styles['container-mobile']}>
+        <div className={styles['grid-left']}>
+          <img src="assets/kassandra-footer.svg" alt="" />
+          <div className={styles['social-media']}>
+            <ul>
+              <li><h4>Company</h4></li>
+              <li><Link href="/about" >About</Link></li>
+              <li><Link href="/partners" >Partners</Link></li>
+            </ul>
+            <ul>
+              <li><h4>Products</h4></li>
+              <li><Link href="/#">Kassandra DAO</Link></li>
+              <li><Link href="/#">$HEIM Index</Link></li>
+            </ul>
+            <ul>
+              <li><h4>Legal</h4></li>
+              <li><Link href="/#">Privacy Policy</Link></li>
+              <li><Link href="/#">Terms of Service</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles['grid-right']}>
+          <ul>
+            <li>
+              <a href="https://twitter.com/kassandra_dao" target="_blank" rel="noopener noreferrer">
+                <img src="assets/discord-icon.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/kassandra_dao" target="_blank" rel="noopener noreferrer">
+                <img src="assets/telegram-icon.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/KassandraFinance" target="_blank" rel="noopener noreferrer">
+                <img src="assets/github-icon.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://kassandrafoundation.medium.com/" target="_blank" rel="noopener noreferrer">
+                <img src="assets/medium-icon.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/dao_kassandra" target="_blank" rel="noopener noreferrer">
+                <img src="assets/twitter-icon.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+          <span>© 2021-2021 Kassandra.</span>
+        </div>
+      </div>
     </footer>
   )
 
