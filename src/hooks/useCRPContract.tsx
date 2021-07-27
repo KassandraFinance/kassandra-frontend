cd687e52885a5831efcf5d5e868d86fae8077b78
Jony <jonyreiscardoso@gmail.com>
2021-07-26 23:18:45 -0400

    feat: add useCRPContract hook

---
+++ src/hooks/useCRPContract.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import BigNumber from 'bn.js'
+import { useSelector, RootStateOrAny } from 'react-redux'
+import { AbiItem } from "web3-utils"
+
+import web3 from '../utils/web3'
+import ConfigurableRightsPool from "../constants/abi/ConfigurableRightsPool.json"
+
+const usePoolContract = () => {
+const { userWalletAddress } = useSelector((state: RootStateOrAny) => state)
+
+  const getCRPContract = (address: string) => {
+    const contract = new web3.eth.Contract((ConfigurableRightsPool as unknown) as AbiItem, address)
+    return contract
+  }
+
+  const exitPool = (addressCRP: string, poolAmountIn, minAmountsOut: Array<BigNumber>) => {
+    console.log(poolAmountIn)
+    const contract = getCRPContract(addressCRP)
+    return contract.methods.exitPool(new BigNumber(poolAmountIn).mul(new BigNumber(10).pow(new BigNumber(18))), minAmountsOut).send({ from: userWalletAddress })
+  }
+
+
+
+  return { 
+    getCRPContract,
+    exitPool
+  }
+}
+
+export default usePoolContract
