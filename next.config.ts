0b30f04d68a10546291a35df6d7dfaa906e7abe7
Jony <jonyreiscardoso@gmail.com>
2021-07-27 22:37:08 -0400

    refactor: fix "Using webpack 4. Reason: future.webpack5 option not enabled"

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: false,
+}
+  
+module.exports = nextConfig
