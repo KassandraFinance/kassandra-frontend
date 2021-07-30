9344a8493860e0353d42a61861c0c1d75f73d19b
Jony <jonyreiscardoso@gmail.com>
2021-07-30 16:29:06 -0400

    feat: update numerals function

--- src/utils/numerals.ts
+++ src/utils/numerals.ts
@@ -1,8 +1,10 @@
 import BigNumber from 'bn.js'
 
 
-export function BNtoDecimal(number: BigNumber, decimals: BigNumber, maxPrecision: number): string {
+export function BNtoDecimal(number: BigNumber, decimalsBN: BigNumber, maxPrecision: number): string {
   const numString = number.toString(10)
+  const decimals = Number(decimalsBN)
+  
   if (numString.length > decimals) {
     const decimalPoints = numString.substring(numString.length - decimals).replace(/0+$/, '').substring(0, maxPrecision)
     return `${numString.substring(0, numString.length - decimals)}${decimalPoints.length > 0 ? '.' : ''}${decimalPoints}`
