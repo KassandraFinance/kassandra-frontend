d423f7f3074188da2b294897adbaaff9706fb5e6
Jony <jonyreiscardoso@gmail.com>
2021-08-27 23:13:31 -0400

    feat: add date function

---
+++ src/utils/date.ts
@@ -0,0 +1,11 @@
+export function getDate(date: any): string {
+  const thisDate = new Date(date * 1000)
+  const dd = thisDate.getDate()
+  const mm = thisDate.getMonth()
+  const yyyy = thisDate.getFullYear()
+  // const hours = today.getHours()
+  // const minutes = today.getMinutes()
+  // const second = today.getSeconds()
+
+  return `${dd}/${mm}/${yyyy}`
+}
