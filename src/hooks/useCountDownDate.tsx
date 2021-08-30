0045d8c66811c1c95cf75d37b0ee72009b8e8f18
Jony <jonyreiscardoso@gmail.com>
2021-08-29 22:48:42 -0400

    feat: add useCountDownDate hook

---
+++ src/hooks/useCountDownDate.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const useCountDownDate = () => {
+  const [date, setDate] = React.useState<string>('')
+
+  function countDown(countDownDate: any) {
+    let x = setInterval(() => {
+  
+      // Get today's date and time
+      let now = new Date().getTime();
+        
+      // Find the distance between now and the count down date
+      let distance = countDownDate - now;
+        
+      // Time calculations for days, hours, minutes and seconds
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      // Output the result in an element with id="demo"
+
+      setDate(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        setDate('')
+      }
+    }, 1000)
+
+  }
+  
+  return {
+    date,
+    countDown
+  }
+}
+
+export default useCountDownDate
