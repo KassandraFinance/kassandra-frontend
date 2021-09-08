87846e3a23231fa6229ac7519738cd3df354f08b
Jony <jonyreiscardoso@gmail.com>
2021-09-08 17:27:15 -0400

    feat: add Matomo on Kassandra Home page

--- src/templates/Home/Hero/index.tsx
+++ src/templates/Home/Hero/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
+import { useMatomo } from '@datapunt/matomo-tracker-react'
 
 import * as S from './styles'
 
 const Hero = () => {
+  const { trackEvent } = useMatomo();
+
+  function clickMatomoEvent(action: string, name: string) {
+    trackEvent({
+      category: "kassandra-page",
+      action: action,
+      name: name,
+    });
+  }
+
   return (
     <S.Hero>
       <div>
@@ -14,12 +25,19 @@ const Hero = () => {
             data-driven investment funds.
           </h3>
           <S.ButtonContainer>
-            <Link href="/heim">View the $HEIM Index</Link>
+            <Link href="/heim" >
+              <a 
+                onClick={() => clickMatomoEvent("click-to-heim", "heim-hero")}
+                >
+                  View the $HEIM Index
+                </a>
+            </Link>
             <S.WithpaperButton
               className="withepaper"
               href="https://drive.google.com/file/d/12jxIMtBVqaY7bMbLmt52Lo0xDFUURZsb/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
+              onClick={() => clickMatomoEvent("click-to-whitepaper", "whitepaper-hero")}
             >
               Whitepaper
             </S.WithpaperButton>
@@ -33,11 +51,18 @@ const Hero = () => {
           data-driven investment funds.
         </h3>
         <S.ButtonContainer>
-          <Link href="/heim">View the $HEIM Index</Link>
+          <Link href="/heim" >
+              <a 
+                onClick={() => clickMatomoEvent("click-to-heim", "heim-hero")}
+                >
+                  View the $HEIM Index
+                </a>
+            </Link>
           <S.WithpaperButton
             href="https://drive.google.com/file/d/12jxIMtBVqaY7bMbLmt52Lo0xDFUURZsb/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={() => clickMatomoEvent("click-to-whitepaper", "whitepaper-hero")}
           >
             Whitepaper
           </S.WithpaperButton>
