ed04cf76fbfd9cb03ce791129b525c20d7ad8a3d
Jony <jonyreiscardoso@gmail.com>
2021-09-08 16:18:01 -0400

    refactor: update link discord

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => (
           <img src="assets/kassandra-footer.svg" alt="kassandra" width="266" height="26" />
           <ul>
             <li>
-              <a href="https://discord.gg/UjRr28Kf" target="_blank" rel="noopener noreferrer">
+              <a href="https://discord.com/invite/2uGEvqNnuq" target="_blank" rel="noopener noreferrer">
                 <img src="assets/discord-icon.svg" alt="Join our Discord community" width="32" height="32" />
               </a>
             </li>
