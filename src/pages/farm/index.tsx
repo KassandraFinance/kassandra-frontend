9517c56ce4947a1f5fc591b68828e5c26186ce7a
Jony <jonyreiscardoso@gmail.com>
2021-09-12 23:06:38 -0400

    feat: add getVotingPower function on Farm page

--- src/pages/farm/index.tsx
+++ src/pages/farm/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import BigNumber from 'bn.js'
 import { useSelector, RootStateOrAny } from 'react-redux'
 
-import styled from 'styled-components'
+import web3 from '../../utils/web3'
+import { BNtoDecimal } from '../../utils/numerals'
+
 
 import useConnect from '../../hooks/useConnect'
 import useStakingContract from '../../hooks/useStakingContract'
 
 import VotingPower from '../../components/VotingPower'
 import OthersStakingPools from '../../components/OthersStakingPools'
-import { BNtoDecimal } from '../../utils/numerals'
+
+import styled from 'styled-components'
 
 const Farm = () => {
   const [totalVotes, setTotalVotes] = React.useState<BigNumber>(new BigNumber(0))
@@ -19,47 +22,60 @@ const Farm = () => {
   const { connect } = useConnect()
   const { getTotalVotes, getCurrentVotes } = useStakingContract()
 
-  React.useEffect(() => {
-    (async () => {
+
+  function getVotingPower() {
+    if (!web3.currentProvider) {
+      return
+    }
+
+    setInterval(async () => {
       const totalVotes = await getTotalVotes()
       setTotalVotes(totalVotes)
       if (userWalletAddress) {
         const currentVotes = await getCurrentVotes(userWalletAddress)
         setYourVotingPower(currentVotes)
       }
-    })()
-  }, [])
+    }, 8000)
+  }
+
+  React.useEffect(() => {
+    getVotingPower()
+  }, [userWalletAddress])
 
   return (
     <FarmContainer>
-    <h1>Staking</h1>
-    <h3>Stake $KACY for Voting Power</h3>
-    <GridStaking>
-      <VotingPower days='0' percentage="12" pid={0} connect={connect} />
-      <VotingPower days='30' percentage="20" pid={1} connect={connect} />
-      <VotingPower days='45' percentage="32" pid={2} connect={connect} />
-    </GridStaking>
-    <TotalVoting>
-      <fieldset>
-        <legend>Your voting power</legend>
-        <span>{BNtoDecimal(new BigNumber(yourVotingPower), new BigNumber(18), 6)}</span>
-      </fieldset>
-      <fieldset>
-        <legend>Total voting power</legend>
-        <span>{BNtoDecimal(new BigNumber(totalVotes), new BigNumber(18), 6)}</span>
-      </fieldset>
-    </TotalVoting>
-    <h3>Other Staking Pools</h3>
-    <GridStaking>
-      <OthersStakingPools img="logo-heim" />
-      <OthersStakingPools img="heim-index" />
-      <OthersStakingPools img="" />
-    </GridStaking>
-  </FarmContainer>
+      <h1>Staking</h1>
+      <h3>Stake $KACY for Voting Power</h3>
+      <GridStaking>
+        <VotingPower days='0' percentage="12" pid={0} connect={connect} />
+        <VotingPower days='30' percentage="20" pid={1} connect={connect} />
+        <VotingPower days='45' percentage="32" pid={2} connect={connect} />
+      </GridStaking>
+      <TotalVoting>
+        <fieldset>
+          <legend>Your voting power</legend>
+          <span>{BNtoDecimal(new BigNumber(yourVotingPower), new BigNumber(18), 6)}</span>
+        </fieldset>
+        <fieldset>
+          <legend>Total voting power</legend>
+          <span>{BNtoDecimal(new BigNumber(totalVotes), new BigNumber(18), 6)}</span>
+        </fieldset>
+      </TotalVoting>
+      <h3>Other Staking Pools</h3>
+      <GridStaking>
+        <OthersStakingPools img="logo-heim" />
+        <OthersStakingPools img="heim-index" />
+        <OthersStakingPools img="" />
+      </GridStaking>
+    </FarmContainer>
   )
 }
 
 const FarmContainer = styled.section`
+  max-width: 1520px;
+  margin: 40px auto 64px;
+  padding: 0 32px;
+
   h1 {
     font-size: 24px;
     font-weight: 400;
@@ -81,7 +97,6 @@ const GridStaking = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 16px;
-  margin: 40px 0 64px;
   @media (max-width: 1160px) {
     grid-template-columns: 1fr 1fr;
     gap: 32px;
