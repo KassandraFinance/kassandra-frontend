9848318b4467fa606a33f746b2cd4af1caacf2a9
Jony <jonyreiscardoso@gmail.com>
2021-09-15 17:13:25 -0400

    feat: add KacyEarned component on VontingPower

---
+++ src/components/VotingPower/KacyEarned/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const KacyEarned = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 2px 0;
+  }
+
+  span {
+    font-size: 12px;
+    font-weight: 300;
+  }
+`
