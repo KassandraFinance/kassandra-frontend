fabc577490726e16e5642d2b254e0b8c385d67db
Jony <jonyreiscardoso@gmail.com>
2021-09-16 18:09:45 -0400

    refactor: update styled on Details component

--- src/components/Details/styles.ts
+++ src/components/Details/styles.ts
@@ -1,36 +1,66 @@
 import styled from 'styled-components'
 
+export const Details = styled.div`
+  margin-top: 24px;
+  width: 100%;
+
+`
+
 export const Info = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  span {
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 14px;
+    margin-bottom: 8px;
+  }
 
-  p {
-    font-size: 18px;
-    line-height: 18px;
-    margin: 24px 0;
-    @media (max-width: 540px) {
-      font-size: 16px;
-      line-height: 16px;
+  .total-staked {
+    margin-bottom: 16px;
+  }
+
+  a {
+    color: #26DBDB;
+    font-size: 14px;
+    text-decoration: none;
+
+    cursor: pointer;
+    z-index: 111;
+    &::after {
+      display: block;
+      content: '';
+      background-color: #26DBDB;
+      width: 100%;
+      height: 0.8px;
+      box-shadow: 1px 1px 4px #26DBDB;
     }
   }
+`
 
-  p:last-child {
-    font-weight: 300;
-  }
+export const KacyUSD = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 
   span {
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 16px;
-    margin-bottom: 8px;
-    display: block;
-    @media (max-width: 540px) {
-      font-size: 13px;
-      line-height: 13px;
-    }
+    margin: 0;
   }
 
-  span:last-child {
-    font-weight: 300;
+  .usd {
+    font-size: 13px;
+    line-height: 13px;
+    margin: 4px 0 8px;
+  }
+`
+
+export const Link = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 8px;
+  img {
+    margin-left: 6px;
   }
 `
