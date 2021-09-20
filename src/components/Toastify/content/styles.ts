c55b28b0d530747f60f508b4b5fe039fab3af593
F-rigo <f.rigo.dev@gmail.com>
2021-09-20 12:12:41 -0300

    style: add theme on component Tostify/Content  style

--- src/components/Toastify/content/styles.ts
+++ src/components/Toastify/content/styles.ts
@@ -1,30 +1,34 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const NotificationContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex: 1;
-  padding: 4px;
+  ${({ theme }) => css`
+    display: flex;
+    align-items: flex-start;
+    flex: 1;
+    padding: 4px;
 
-  font-family: 'Rubik';
-  img {
-    margin-right: 12px;
-  }
-` 
+    font-family: 'Rubik';
+    img {
+      margin-right: 12px;
+    }
+  `}
+`
 
 export const MessageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  font-family: 'Rubik';
-  h3 {
-    font-size: 16px;
-    font-weight: 500;
-    margin-bottom: 8px; 
-  }
-  span {
-    font-size: 16px;
-    font-weight: 300;
-    margin: auto;
-  }
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    font-family: 'Rubik';
+    h3 {
+      font-size: ${theme.font.sizes.font16};
+      font-weight: ${theme.font.weight.medium};
+      margin-bottom: 8px;
+    }
+    span {
+      font-size: ${theme.font.sizes.font16};
+      font-weight: ${theme.font.weight.light};
+      margin: auto;
+    }
+  `}
 `
