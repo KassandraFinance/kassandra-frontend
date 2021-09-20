26a451e7dea2380a9976ec9c69f868f6b3d81020
F-rigo <f.rigo.dev@gmail.com>
2021-09-20 12:32:50 -0300

    style: add theme on template Home/KassandraArchitecture  style

--- src/templates/Home/KassandraArchitecture/styles.ts
+++ src/templates/Home/KassandraArchitecture/styles.ts
@@ -1,42 +1,68 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const KassandraArchitecture = styled.section`
-  text-align: center;
-  padding: 80px 32px;
-  max-width: 1520px;
-  margin: 0 auto;
-  @media (max-width: 320px) {
-    padding: 40px 20px;
-  }
-  h1 {
-    font-size: 40px;
-    font-weight: 500;
+  ${({ theme }) => css`
     text-align: center;
-    @media (max-width: 800px) {
-      font-size: 32px;
+    padding: 80px 32px;
+    max-width: 1520px;
+    margin: 0 auto;
+    @media (max-width: 320px) {
+      padding: 40px 20px;
     }
-    @media (max-width: 500px) {
-      font-size: 24px;
+    h1 {
+      font-size: ${theme.font.sizes.font40};
+      font-weight: ${theme.font.weight.medium};
+      text-align: center;
+      @media (max-width: 800px) {
+        font-size: ${theme.font.sizes.font32};
+      }
+      @media (max-width: 500px) {
+        font-size: ${theme.font.sizes.font24};
+      }
     }
-  }
-  p {
-    font-size: 24px;
-    font-weight: 300;
-    text-align: center;
-    max-width: 1000px;
-    margin: 48px auto;
-    @media (max-width: 800px) {
-      font-size: 18px;
+    p {
+      font-size: ${theme.font.sizes.font24};
+      font-weight: ${theme.font.weight.light};
+      text-align: center;
+      max-width: 1000px;
+      margin: 48px auto;
+      @media (max-width: 800px) {
+        font-size: ${theme.font.sizes.font18};
+      }
+      @media (max-width: 500px) {
+        font-size: ${theme.font.sizes.font16};
+        margin: ${theme.spacings.space16} auto 32px;
+      }
     }
-    @media (max-width: 500px) {
-      font-size: 16px;
-      margin: 16px auto 32px;
+    .img-kassandra-architecture {
+      max-width: 100%;
     }
-  }
-  .img-kassandra-architecture {
-    max-width: 100%;
-  }
-  .grid-paragraph {
+    .grid-paragraph {
+      display: grid;
+      grid-template-columns: 4fr 5fr;
+      gap: 80px;
+      margin: 64px 0;
+      @media (max-width: 800px) {
+        grid-template-columns: 1fr;
+        gap: 0px;
+      }
+      p {
+        font-size: ${theme.font.sizes.font18};
+        text-align: left;
+        margin: 8px 0;
+        @media (max-width: 800px) {
+          font-size: ${theme.font.sizes.font16};
+        }
+        @media (max-width: 500px) {
+          font-size: 15px;
+        }
+      }
+    }
+  `}
+`
+
+export const Text = styled.div`
+  ${({ theme }) => css`
     display: grid;
     grid-template-columns: 4fr 5fr;
     gap: 80px;
@@ -46,37 +72,15 @@ export const KassandraArchitecture = styled.section`
       gap: 0px;
     }
     p {
-      font-size: 18px;
+      font-size: ${theme.font.sizes.font18};
       text-align: left;
       margin: 8px 0;
       @media (max-width: 800px) {
-        font-size: 16px;
+        font-size: ${theme.font.sizes.font16};
       }
       @media (max-width: 500px) {
         font-size: 15px;
       }
     }
-  }
-`
-
-export const Text = styled.div`
-  display: grid;
-  grid-template-columns: 4fr 5fr;
-  gap: 80px;
-  margin: 64px 0;
-  @media (max-width: 800px) {
-    grid-template-columns: 1fr;
-    gap: 0px;
-  }
-  p {
-    font-size: 18px;
-    text-align: left;
-    margin: 8px 0;
-    @media (max-width: 800px) {
-      font-size: 16px;
-    }
-    @media (max-width: 500px) {
-      font-size: 15px;
-    }
-  }
+  `}
 `
