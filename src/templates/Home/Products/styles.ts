29311145eb2f70a67cf1ad9556a441776132b898
F-rigo <f.rigo.dev@gmail.com>
2021-09-20 12:33:22 -0300

    style: add theme on template home/Products  style

--- src/templates/Home/Products/styles.ts
+++ src/templates/Home/Products/styles.ts
@@ -1,58 +1,60 @@
 import styled, { css } from 'styled-components'
-import theme from '../../../styles/theme'
 
 export const Products = styled.section`
-  padding: 80px 32px;
-  margin: 0 auto;
-  max-width: 1520px;
-
-  @media (min-width: 900px) {
-    padding: 80px 32px;
-  }
-
-  @media (max-width: 375px) and (max-height: 800px) {
-    padding-top: 100px;
-  }
-  @media (max-width: 370px) {
-    padding-top: 120px;
-  }
-  @media (max-width: 330px) {
-    padding: 180px 20px 0;
-  }
-  h1 {
-    font-size: 40px;
-    font-weight: 500;
-    text-align: center;
-
-    margin-bottom: 48px;
-  }
-  p {
-    font-size: 24px;
-    font-weight: 300;
-    max-width: 700px;
+  ${({ theme }) => css`
+    padding: 80px ${theme.spacings.space32};
     margin: 0 auto;
-    text-align: center;
-    @media (max-width: 600px) {
-      font-size: 22px;
+    max-width: 1520px;
+
+    @media (min-width: 900px) {
+      padding: 80px ${theme.spacings.space32};
     }
-    @media (max-width: 500px) {
-      font-size: 20px;
+
+    @media (max-width: 375px) and (max-height: 800px) {
+      padding-top: 100px;
     }
-  }
-  .more-icon {
-    display: block;
-    margin: 16px auto;
-  }
-  .more-text {
-    font-size: 20px;
-    font-weight: 500;
-    text-align: center;
-    margin: 16px auto;
-  }
+    @media (max-width: 370px) {
+      padding-top: 120px;
+    }
+    @media (max-width: 330px) {
+      padding: 180px 20px 0;
+    }
+    h1 {
+      font-size: ${theme.font.sizes.font40};
+      font-weight: ${theme.font.weight.medium};
+      text-align: center;
+
+      margin-bottom: ${theme.spacings.space48}
+;
+    }
+    p {
+      font-size: ${theme.font.sizes.font24};
+      font-weight: ${theme.font.weight.light};
+      max-width: 700px;
+      margin: 0 auto;
+      text-align: center;
+      @media (max-width: 600px) {
+        font-size: 22px;
+      }
+      @media (max-width: 500px) {
+        font-size: ${theme.font.sizes.font20};
+      }
+    }
+    .more-icon {
+      display: block;
+      margin: ${theme.spacings.space16} auto;
+    }
+    .more-text {
+      font-size: ${theme.font.sizes.font20};
+      font-weight: ${theme.font.weight.medium};
+      text-align: center;
+      margin: ${theme.spacings.space16} auto;
+    }
+  `}
 `
 
 export const Heim = styled.div`
-  ${({theme}) => css`
+  ${({ theme }) => css`
     border: none;
     border-radius: 50px;
     box-shadow:
@@ -68,7 +70,7 @@ export const Heim = styled.div`
     justify-items: center;
     align-items: center;
 
-    margin-top: 48px;
+    margin-top: ${theme.spacings.space48};
     padding: 56px 64px;
     position: relative;
     img {
@@ -79,7 +81,7 @@ export const Heim = styled.div`
     }
     .arrow-right {
       color: ${theme.colors.cyan};
-      font-size: 16px;
+      font-size: ${theme.font.sizes.font16};
       text-decoration: none;
       @media (max-width: 420px) {
         margin: 20px 0;
@@ -88,7 +90,7 @@ export const Heim = styled.div`
         text-decoration: underline;
       }
       img {
-        margin-left: 8px;
+        margin-left: ${theme.spacings.space8};
       }
     }
     @media (max-width: 880px) {
@@ -96,111 +98,118 @@ export const Heim = styled.div`
       gap: 40px;
     }
     @media (max-width: 700px) {
-      padding: 32px 56px;
+      padding: ${theme.spacings.space32} 56px;
     }
     @media (max-width: 500px) {
-      padding: 32px 48px;
+      padding: ${theme.spacings.space32} 48px;
     }
     @media (max-width: 420px) {
-      padding: 32px;
+      padding: ${theme.spacings.space32};
     }
   `}
 `
 
 export const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  height: 100%;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
 
-  a {
-    background: linear-gradient(264.12deg, #E843C4 -179.71%, #020887 205.21%);
-    border: none;
-    border-radius: 6px;
-    color: #fcfcfc;
-    text-align: center;
-    text-decoration: none;
-    font-size: 20px;
-    line-height: 20px;
+    a {
+      background ${`linear-gradient(264.12deg, ${theme.colors.magenta} -179.71%, ${theme.colors.darkBlue} 205.21%)`};
+      border: none;
+      border-radius: ${theme.border.radius};
+      color: ${theme.colors.snow};
+      text-align: center;
+      text-decoration: none;
+      font-size: ${theme.font.sizes.font20};
+      line-height: 20px;
 
-    height: 52px;
-    padding: 16px 32px;
-    margin: 16px 0;
-    cursor: pointer;
-    &:hover {
-      background: #020887;
+      height: 52px;
+      padding: ${theme.spacings.space16} 32px;
+      margin: ${theme.spacings.space16} 0;
+      cursor: pointer;
+      &:hover {
+        background: #020887;
+      }
+      @media (max-width: 600px) {
+        font-size: ${theme.font.sizes.font16};
+        line-height: 16px;
+        height: 48px;
+      }
     }
-    @media (max-width: 600px) {
-      font-size: 16px;
-      line-height: 16px;
-      height: 48px;
-    }
-  }
+  `}
 `
 
 export const LearnMore = styled.a`
-  background: transparent !important;
-  border: 1px solid ${theme.colors.cyan} !important;
-  border-radius: 6px;
-  color: #fcfcfc !important;
-  text-decoration: none;
-  font-size: 20px;
+  ${({ theme }) => css`
+    background: transparent !important;
+    border: 1px solid ${theme.colors.cyan} !important;
+    border-radius: ${theme.border.radius};
+    color: ${theme.colors.snow} !important;
+    text-decoration: none;
+    font-size: ${theme.font.sizes.font20};
 
-  height: 52px;
-  padding: 12px 32px;
-  cursor: pointer;
-  transition-duration: 300ms;
-  &:hover {
-    background-color: ${theme.colors.cyan} !important;
-    color: #211426 !important;
-  }
-  @media (max-width: 600px) {
-    font-size: 16px;
-    height: 48px;
-  }
+    height: 52px;
+    padding: 12px 32px;
+    cursor: pointer;
+    transition-duration: 300ms;
+    &:hover {
+      background-color: ${theme.colors.cyan} !important;
+      color: ${theme.colors.darkPurple}
+ !important;
+    }
+    @media (max-width: 600px) {
+      font-size: ${theme.font.sizes.font16};
+      height: 48px;
+    }
+  `}
 `
 
 export const HeimDescription = styled.div`
-  text-align: left;
-  h3 {
-    font-size: 24px;
-    font-weight: 500;
-  }
-  p {
-    font-size: 18px;
+  ${({ theme }) => css`
     text-align: left;
+    h3 {
+      font-size: ${theme.font.sizes.font24};
+      font-weight: ${theme.font.weight.medium};
+    }
+    p {
+      font-size: ${theme.font.sizes.font18};
+      text-align: left;
 
-    max-width: 600px;
-    margin: 16px 0;
-  }
-  .first-paragraph {
-    font-weight: 300;
-  }
-  .second-paragraph {
-    font-weight: 500;
-  }
-  @media (max-width: 960px) {
-    h3 {
-      font-size: 28px;
+      max-width: 600px;
+      margin: ${theme.spacings.space16} 0;
     }
-    p {
-      font-size: 18px;
+    .first-paragraph {
+      font-weight: ${theme.font.weight.light};
     }
-  }
-  @media (max-width: 880px) {
-    h3 {
-      font-size: 32px;
+    .second-paragraph {
+      font-weight: ${theme.font.weight.medium};
     }
-    p {
-      font-size: 22px;
+    @media (max-width: 960px) {
+      h3 {
+        font-size: 28px;
+      }
+      p {
+        font-size: ${theme.font.sizes.font18};
+      }
     }
-  }
-  @media (max-width: 540px) {
-    h3 {
-      font-size: 20px;
+    @media (max-width: 880px) {
+      h3 {
+        font-size: ${theme.font.sizes.font32};
+      }
+      p {
+        font-size: 22px;
+      }
     }
-    p {
-      font-size: 16px;
+    @media (max-width: 540px) {
+      h3 {
+        font-size: ${theme.font.sizes.font20};
+      }
+      p {
+        font-size: ${theme.font.sizes.font16};
+      }
     }
-  }
+  `}
 `
