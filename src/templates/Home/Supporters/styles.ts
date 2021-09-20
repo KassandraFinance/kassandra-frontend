e1d3cffd39c0994f7fdf24b6fb66c213428b48c4
F-rigo <f.rigo.dev@gmail.com>
2021-09-20 12:33:37 -0300

    style: add theme on template Home/Supporters  style

--- src/templates/Home/Supporters/styles.ts
+++ src/templates/Home/Supporters/styles.ts
@@ -1,28 +1,29 @@
 import styled from 'styled-components'
+import theme from '../../../styles/theme'
 
 export const Supporters = styled.section`
   padding: 80px 32px;
   max-width: 1520px;
   margin: 0 auto;
   h1 {
-    font-size: 40px;
-    font-weight: 500;
+    font-size: ${theme.font.sizes.font40};
+    font-weight: ${theme.font.weight.medium};
     text-align: center;
     @media (max-width: 600px) {
-      font-size: 24px;
+      font-size: ${theme.font.sizes.font24};
     }
   }
 
   p {
-    font-size: 24px;
-    font-weight: 300;
+    font-size: ${theme.font.sizes.font24};
+    font-weight: ${theme.font.weight.light};
     max-width: 700px;
     margin: 36px auto 0;
     text-align: center;
     @media (max-width: 600px) {
-      font-size: 16px;
+      font-size: ${theme.font.sizes.font16};
       max-width: 280px;
-      margin: 16px auto 0;
+      margin: ${theme.spacings.space16} auto 0;
     }
   }
 `
