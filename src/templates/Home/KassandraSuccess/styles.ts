3ee5058e2fbf05c4092522ddfdb9cec5039ab508
F-rigo <f.rigo.dev@gmail.com>
2021-09-20 12:33:06 -0300

    style: add theme on template home/kassandraSuccess  style

--- src/templates/Home/KassandraSuccess/styles.ts
+++ src/templates/Home/KassandraSuccess/styles.ts
@@ -1,39 +1,41 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const KassandraSuccess = styled.section`
-  padding: 80px 32px 120px;
-  max-width: 1220px;
-  margin: 0 auto;
-  @media (min-width: 1880px) {
-    padding: 80px 0 240px;
-  }
-  @media (min-width: 2200px) {
-    padding: 80px 0 320px;
-  }
-  h1 {
-    font-size: 40px;
-    font-weight: 500;
-    text-align: center;
-    @media (max-width: 600px) {
-      font-size: 24px;
+  ${({ theme }) => css`
+    padding: 80px 32px 120px;
+    max-width: 1220px;
+    margin: 0 auto;
+    @media (min-width: 1880px) {
+      padding: 80px 0 240px;
     }
-  }
-  p {
-    font-size: 24px;
-    font-weight: 300;
-    text-align: center;
-    max-width: 1000px;
-    margin: 48px auto;
-    @media (max-width: 800px) {
-      font-size: 18px;
+    @media (min-width: 2200px) {
+      padding: 80px 0 320px;
     }
-    @media (max-width: 500px) {
-      font-size: 16px;
+    h1 {
+      font-size: ${theme.font.sizes.font40};
+      font-weight: ${theme.font.weight.medium};
+      text-align: center;
+      @media (max-width: 600px) {
+        font-size: ${theme.font.sizes.font24};
+      }
     }
-  }
-  img {
-    margin: 0;
-  }
+    p {
+      font-size: ${theme.font.sizes.font24};
+      font-weight: ${theme.font.weight.light};
+      text-align: center;
+      max-width: 1000px;
+      margin: 48px auto;
+      @media (max-width: 800px) {
+        font-size: ${theme.font.sizes.font18};
+      }
+      @media (max-width: 500px) {
+        font-size: ${theme.font.sizes.font16};
+      }
+    }
+    img {
+      margin: 0;
+    }
+  `}
 `
 
 export const Success = styled.div`
@@ -90,74 +92,76 @@ export const Success = styled.div`
 `
 
 export const SuccessItem = styled.div`
-  text-align: center;
-  padding: 32px;
-  background-repeat: no-repeat;
-  background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-tr.svg);
-  background-position: 0 0, 100% 0;
-  width: 100%;
-  h2 {
-    font-size: 24px;
-    margin-top: 16px;
-    @media (max-width: 600px) {
-      font-size: 20px;
+  ${({ theme }) => css`
+    text-align: center;
+    padding: ${theme.spacings.space32};
+    background-repeat: no-repeat;
+    background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-tr.svg);
+    background-position: 0 0, 100% 0;
+    width: 100%;
+    h2 {
+      font-size: ${theme.font.sizes.font24};
+      margin-top: 16px;
+      @media (max-width: 600px) {
+        font-size: ${theme.font.sizes.font20};
+      }
     }
-  }
-  p {
-    font-size: 18px;
-    margin: 20px auto 48px;
-    max-width: 280px;
-    @media (max-width: 1024px) {
-      max-width: 380px;
+    p {
+      font-size: ${theme.font.sizes.font18};
+      margin: 20px auto 48px;
+      max-width: 280px;
+      @media (max-width: 1024px) {
+        max-width: 380px;
+      }
+      @media (max-width: 600px) {
+        font-size: ${theme.font.sizes.font16};
+      }
+      @media (max-width: 370px) {
+        font-size: 14px;
+        margin: 20px 0 32px;
+      }
     }
-    @media (max-width: 600px) {
-      font-size: 16px;
-    }
-    @media (max-width: 370px) {
-      font-size: 14px;
-      margin: 20px 0 32px;
-    }
-  }
-  &:first-child {
-    background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-br.svg);
-    background-position: 0 0, 0 100%, 100% 100%;
-  }
-  &:last-child {
-    background-image: url(/assets/group-success-tr.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-br.svg);
-    background-position: 100% 0, 0 100%, 100% 100%;
-  }
-  @media (max-width: 1024px) {
-    background-image: url(/assets/group-success-br.svg), url(/assets/group-success-tr.svg);
-    background-position: 100% 100%, 100% 0;
     &:first-child {
-      background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-tr.svg);
-      background-position: 0 0, 0 100%, 100% 0;
-    }
-    &:last-child {
       background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-br.svg);
       background-position: 0 0, 0 100%, 100% 100%;
     }
-  }
-  @media (max-width: 420px) {
-    padding: 40px;
-  }
-  @media (max-width: 370px) {
-    padding: 24px;
-  }
-
-  img {
-    max-width: 150px;
-    @media (max-width: 1024px) {
-      max-width: 140px;
+    &:last-child {
+      background-image: url(/assets/group-success-tr.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-br.svg);
+      background-position: 100% 0, 0 100%, 100% 100%;
     }
-    @media (max-width: 700px) {
-      max-width: 130px;
+    @media (max-width: 1024px) {
+      background-image: url(/assets/group-success-br.svg), url(/assets/group-success-tr.svg);
+      background-position: 100% 100%, 100% 0;
+      &:first-child {
+        background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-tr.svg);
+        background-position: 0 0, 0 100%, 100% 0;
+      }
+      &:last-child {
+        background-image: url(/assets/group-success-tl.svg), url(/assets/group-success-bl.svg), url(/assets/group-success-br.svg);
+        background-position: 0 0, 0 100%, 100% 100%;
+      }
     }
     @media (max-width: 420px) {
-      max-width: 120px;
+      padding: 40px;
     }
-    @media (max-width: 330px) {
-      max-width: 110px;
+    @media (max-width: 370px) {
+      padding: 24px;
     }
-  }
+
+    img {
+      max-width: 150px;
+      @media (max-width: 1024px) {
+        max-width: 140px;
+      }
+      @media (max-width: 700px) {
+        max-width: 130px;
+      }
+      @media (max-width: 420px) {
+        max-width: 120px;
+      }
+      @media (max-width: 330px) {
+        max-width: 110px;
+      }
+    }
+  `}
 `
