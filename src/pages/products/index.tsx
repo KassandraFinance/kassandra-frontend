fd2ac3af5a77375c659a69e1bff860a86675bcf2
Jony <jonyreiscardoso@gmail.com>
2021-09-22 20:29:52 -0400

    feat: add getCoinList function on Products page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,21 +1,78 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSelector, RootStateOrAny } from 'react-redux'
+
 import theme from '../../styles/theme'
 
 import HeimOperations from '../../components/HeimOperations'
 import IndexDetails from '../../components/IndexDetails'
 
-const Products = () => (
-  <>
-    <ProductsContainer>
-      <ComingSoon src="assets/coming-soon.png" alt="coming-soon" />
-      <HeimOperations />
-    </ProductsContainer>
-    <section>
+
+const Products = () => {
+  const [coinInfoList, setCoinInfoList] = React.useState<Array<any>>([])
+  
+  const { poolTokensArray } = useSelector((state: RootStateOrAny) => state)
+
+  async function getCoinList() {
+    const URL = 'https://api.coingecko.com/api/v3/coins/list'
+    await fetch(URL, {
+      method: 'get'
+    })
+      .then(res => res.json())
+      .then(res => res.forEach((item: any) => isTokenPool(item)))
+      .catch(err => err)
+  }
+
+  function isTokenPool(value: any) {
+    for (let i = 0; i < poolTokensArray.length; i++) {
+      let element = poolTokensArray[i];
+      // let newName = element.name.replace("Kassandra Test ", "")
+
+      if (value.symbol === element.symbol.toLowerCase()) {
+        if (value.symbol === "uni" && value.name !== "Uniswap") {
+          continue
+        }
+        if (value.symbol === "grt" && value.name !== "The Graph") {
+          continue
+        }
+        getCoin(value.id, element.allocation)
+      }
+    }
+  }
+
+  async function getCoin(id: string, allocation: number) {
+    const URL = `https://api.coingecko.com/api/v3/coins/${id}`
+    await fetch(URL, {
+      method: 'get'
+    })
+      .then(res => res.json())
+      .then(res => setCoinInfoList(prevState => [...prevState, {...res, allocation}]))
+      .catch(err => err)
+  }
+
+  React.useEffect(() => {
+    if (poolTokensArray.length < 1) {
+      return
+    }
+    getCoinList()
+    setCoinInfoList([])
+  }, [poolTokensArray])
+
+  return (
+    <>
+      <ProductsContainer>
+        <ComingSoon src="assets/coming-soon.png" alt="coming-soon" />
+        <HeimOperations />
+      </ProductsContainer>
+      <section>
       <IndexDetails />
     </section>
   </>
 )
+      </section>
+    </>
+  )
+}
 
 const ProductsContainer = styled.section`
   max-width: 1520px;
