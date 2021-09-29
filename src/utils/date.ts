d3ec11abed1176a4cff792590676f9311736e97e
Jony <jonyreiscardoso@gmail.com>
2021-09-29 18:34:36 -0400

    feat: add allMonth on date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,18 @@
+const allMonth: any = {
+  0: "Jan",
+  1: "Feb",
+  2: "Mar",
+  3: "Apr",
+  4: "May",
+  5: "Jun",
+  6: "Jul",
+  7: "Ago",
+  8: "Sep",
+  9: "Oct",
+  10: "Nov",
+  11: "Dec"
+}
+
 export function getDate(date: number): string {
   const thisDate = new Date(date * 1000)
   const dd = thisDate.getDate()
@@ -7,5 +22,5 @@ export function getDate(date: number): string {
   // const minutes = today.getMinutes()
   // const second = today.getSeconds()
 
-  return `${dd}/${mm}/${yyyy}`
+  return `${dd} ${allMonth[mm]}, ${yyyy}`
 }
