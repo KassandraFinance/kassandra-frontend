04078bd852d86417412ac6ee873b10f01cbc1bdc
F-rigo <f.rigo.dev@gmail.com>
2021-10-20 21:18:13 -0300

    feat: add subscribeBanner component

--- src/components/SubscribeBanner/styles.ts
+++ src/components/SubscribeBanner/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import theme from '../../styles/theme'
 
+import * as TextFieldStyles from '../../components/TextField/styles'
+
 export const Container = styled.section`
   display: grid;
-  grid-template-columns: minmax(1fr 2fr);
+  grid-template-columns: 1fr 1fr;
   max-width: 760px;
   margin: 40px auto;
 `
@@ -13,6 +15,7 @@ export const TextWrapper = styled.div`
     font-size: ${theme.font.sizes.font40};
     font-weight: ${theme.font.weight.black};
     line-height: 110%;
+    margin-bottom: 16px;
   }
   span{
     font-size: ${theme.font.sizes.font16};
@@ -20,3 +23,26 @@ export const TextWrapper = styled.div`
     line-height: 155%;
   }
 `
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+
+  ${TextFieldStyles.Input} {
+    background-color: #ffff;
+    color: ${theme.colors.darkPurple};
+  }
+  ${TextFieldStyles.InputWrapper} {
+    background-color: #ffff;
+    border-color:#ffff;
+    border-radius: 12px;
+    height: 60px;
+  }
+  ${TextFieldStyles.Icon} {
+    cursor: pointer;
+  }
+   button{
+    border: 'none';
+  }
+  `
