f00ab2df32e6b6aac29ecd2891bac47e026562ff
F-rigo <f.rigo.dev@gmail.com>
2021-10-20 21:18:46 -0300

    feat: add BannerCTA component

---
+++ src/components/BannerCTA/styles.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+import * as ButtonStyles from '../Button/styles'
+
+export const Container = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 94.4rem;
+  margin: auto;
+  margin-block: 11rem 5.5rem;
+  padding: 8rem 6rem;
+
+  border-radius: 1rem;
+  border: 0.1rem solid #FFFFFF0A;
+  background: #2114261F;
+`
+export const ImageWrapper = styled.div`
+  max-width: 80%;
+  img{
+    width: 100%;
+  }
+`
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  p{
+    font-size: ${theme.font.sizes.font12};
+    letter-spacing: 0.4rem;
+    color: #c4c4c4;
+  }
+  h1{
+    font-size: ${theme.font.sizes.font36};
+    line-height: 110%;
+    margin: 2.4rem 0;
+  }
+  span{
+    font-size: ${theme.font.sizes.font16};
+    line-height: 155%;
+    font-weight: ${theme.font.weight.light};
+  }
+`
+export const BannerFooter = styled.div`
+  max-width: 36rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 6rem ;
+
+  ${ButtonStyles.Wrapper} {
+      width: 65%;
+      padding: 1.6rem 2.4rem;
+      font-size: ${theme.font.sizes.font18};
+      font-weight: ${theme.font.weight.medium};
+      height: 5rem;
+    }
+  a{
+    justify-items: center;
+    text-decoration: none;
+    font-size: ${theme.font.sizes.font14};
+    font-weight: ${theme.font.weight.light};
+    color: ${theme.colors.snow};
+    margin-right: ${theme.spacings.space8};
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
