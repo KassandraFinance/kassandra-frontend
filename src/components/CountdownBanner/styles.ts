1a86fe194c077f39ca8cc5eb6ca10515d96aa8dd
F-rigo <f.rigo.dev@gmail.com>
2021-10-29 15:09:01 -0300

    feat: add countdown component

---
+++ src/components/CountdownBanner/styles.ts
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+
+export const Background = styled.div`
+
+  margin: 0 auto 5rem;
+  @media(max-width: 100rem) {
+    text-align: center;
+    padding: 1.6rem 1.6rem;
+  }
+`
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  max-width: 96.5rem;
+  margin: auto;
+  padding: 7.5rem 12rem 4rem;
+
+  border: 1px solid rgba(255, 255, 255, 0.13);
+  border-radius: 1rem;
+  backdrop-filter: blur(10px);
+  background: rgba(33, 20, 38, 0.12);
+
+  @media(max-width: 100rem) {
+    text-align: center;
+    padding: 1.6rem 1.6rem;
+  }
+`
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  p{
+    font-size: ${theme.font.sizes.font12};
+    letter-spacing: 0.4rem;
+    color: ${theme.colors.amber};
+    margin-bottom: 2.4rem;
+  }
+  span{
+    font-size: ${theme.font.sizes.font16};
+    letter-spacing: 0.56rem;
+    font-weight: ${theme.font.weight.light};
+    color: ${theme.colors.amber};
+    margin: 3.8rem 0 2.4rem;
+}
+`
+export const TitleAndImage = styled.div`
+  display: flex;
+  align-items: flex-end;
+  text-align: center;
+  margin: 0 auto;
+  h1{
+    font-size: ${theme.font.sizes.font36};
+    font-weight: ${theme.font.weight.black};
+    line-height: 110%;
+    max-width: 70rem;
+    @media(max-width: 44rem) {
+      font-size: 2.4rem;
+    }
+  }
+  img {
+    max-width: 3rem;
+    margin-left: 0.6rem;
+  }
+`
+export const TimerContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  max-width: 100%;
+`
+export const TimerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 10rem;
+  span {
+    font-size: 6.4rem;
+    letter-spacing: 0.32rem;
+    font-weight: ${theme.font.weight.light};
+    @media(max-width: 44rem) {
+      font-size: 3.6rem;
+    }
+  }
+  p{
+    font-size: ${theme.font.sizes.font18};
+  }
+`
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -6,3 +6,26 @@ export const Background = styled.div`
   background-size: cover;
   background-position: center;
 `
+export const BackgroundSubscribe = styled.div`
+  background: #2114261F;
+  border: 1px solid #FFFFFF21;
+  border-radius: 10px;
+  max-width: 96rem;
+  margin: 0 auto 170px;
+  padding: 40px 60px 0px 60px;
+  @media(max-width: 768px) {
+    display: none;
+  }
+`
+export const BackgoundCountDown = styled.div`
+background: url('assets/backgroundCountdown.svg');
+background-repeat: no-repeat;
+background-size: auto;
+background-position: center;
+padding: 100px 100px;
+@media(max-width: 768px) {
+  background: none;
+  padding: 0;
+}
+
+`
