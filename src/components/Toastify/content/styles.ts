43a9410e8acc1a67f5630edecfad0dbb97e1cbad
Jony <jonyreiscardoso@gmail.com>
2021-10-31 16:16:56 -0400

    style: update style on Toastify component

--- src/components/Toastify/content/styles.ts
+++ src/components/Toastify/content/styles.ts
@@ -1,34 +1,31 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import theme from '../../../styles/theme'
 
 export const NotificationContainer = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    align-items: flex-start;
-    flex: 1;
-    padding: 4px;
+  display: flex;
+  align-items: flex-start;
+  flex: 1;
+  padding: 4px;
 
-    font-family: 'Rubik';
-    img {
-      margin-right: 12px;
-    }
-  `}
+  font-family: 'Rubik';
+  img {
+    margin-right: 12px;
+  }
 `
 
 export const MessageContainer = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    font-family: 'Rubik';
-    h3 {
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.medium};
-      margin-bottom: 8px;
-    }
-    span {
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.light};
-      margin: auto;
-    }
-  `}
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  font-family: 'Rubik';
+  h3 {
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.bold};
+    margin-bottom: 8px;
+  }
+  span {
+    font-size: 15px;
+    font-weight: ${theme.font.weight.light};
+    margin: auto;
+  }
 `
--- src/components/Toastify/styles.ts
+++ src/components/Toastify/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import theme from '../../styles/theme'
 import { ToastContainer } from "react-toastify";
 import toastConfig from "../../config/toastConfig";
@@ -7,73 +7,64 @@ import toastConfig from "../../config/toastConfig";
 export const StyledToastContainer = styled(ToastContainer).attrs({
   ...toastConfig,
 })`
-  ${({ theme }) => css`
-    .Toastify__toast {
-      font-family: 'Rubik';
-      font-weight: ${theme.font.weight.light};
-      width: 352px;
-      min-height: 68px;
-      padding: 0;
-      margin-right: 20px;
-      position: absolute;
-      right: 0;
-    }
-    .Toastify__close-button {
-      position: absolute;
-      right: 12px;
-      top: 12px;
-    }
-    .Toastify__toast--error {
-      border: 1.6px solid #E8372C;
-      border-radius: ${theme.border.radius};
-      background: ${theme.colors.darkPurple}
-;
-      color: ${theme.colors.snow};
+  .Toastify__toast {
+    font-family: 'Rubik';
+    font-weight: ${theme.font.weight.light};
+    width: 352px;
+    min-height: 68px;
+    padding: 0;
+    margin-left: 20px;
+    position: absolute;
+    left: 0;
+    bottom: 0px;
+  }
+  .Toastify__close-button {
+    position: absolute;
+    right: 12px;
+    top: 12px;
+  }
+  .Toastify__toast--error {
+    border-radius: 4px;
+    background: rgba(31, 31, 31, 0.82);
+    color: ${theme.colors.snow};
 
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.light};
-      .Toastify__progress-bar {
-        background: #E8372C;
-      }
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.light};
+    .Toastify__progress-bar {
+      background: #E8372C;
     }
-    .Toastify__toast--success {
-      border: 1.6px solid #2CE878;
-      border-radius: ${theme.border.radius};
-      background: ${theme.colors.darkPurple}
-;
-      color: ${theme.colors.snow};
+  }
+  .Toastify__toast--success {
+    border-radius: 4px;
+    background: rgba(31, 31, 31, 0.82);
+    color: ${theme.colors.snow};
 
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.light};
-      .Toastify__progress-bar {
-        background: #2CE878;
-      }
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.light};
+    .Toastify__progress-bar {
+      background: #2CE878;
     }
-    .Toastify__toast--warning {
-      border: 1.6px solid #FFBF00;
-      border-radius: ${theme.border.radius};
-      background: ${theme.colors.darkPurple}
-;
-      color: ${theme.colors.snow};
+  }
+  .Toastify__toast--warning {
+    border-radius: 4px;
+    background: rgba(31, 31, 31, 0.82);
+    color: ${theme.colors.snow};
 
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.light};
-      .Toastify__progress-bar {
-        background: #FFBF00;
-      }
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.light};
+    .Toastify__progress-bar {
+      background: #FFBF00;
     }
-    .Toastify__toast--info {
-      border: 1.6px solid ${theme.colors.cyan};
-      border-radius: ${theme.border.radius};
-      background: ${theme.colors.darkPurple}
-;
-      color: ${theme.colors.snow};
+  }
+  .Toastify__toast--info {
+    border-radius: 4px;
+    background: rgba(31, 31, 31, 0.82);
+    color: ${theme.colors.snow};
 
-      font-size: ${theme.font.sizes.font16};
-      font-weight: ${theme.font.weight.light};
-      .Toastify__progress-bar {
-        background: ${theme.colors.cyan};
-      }
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.light};
+    .Toastify__progress-bar {
+      background: ${theme.colors.cyan};
     }
-  `}
+  }
 `;
