88d996409d65fec9361342bbab6f6d8cb25544ea
F-rigo <f.rigo.dev@gmail.com>
2021-11-03 11:40:02 -0300

    feat: add button to top on homepage

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import theme from '../../styles/theme'
+
 
 export const Background = styled.div`
   background: url('assets/latestBackground.svg');
@@ -26,6 +28,39 @@ padding: 100px 100px;
 @media(max-width: 768px) {
   background: none;
   padding: 0;
-}
+}`
 
+export const ScrollUpButton = styled.div`
+
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 60px;
+  padding: 16px;
+  height: 60px;
+  text-align: center;
+  line-height: 60px;
+  background: ${theme.colors.darkPurple};
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    border: 1px solid ${theme.colors.darkPurple};
+    background: ${theme.colors.snow};
+
+  }
+  @media(max-width: 768px) {
+    display: none;
+  }
+  `
+//ScrollUpIcon
+export const ScrollUpIcon = styled.div`
+  height: 100%;
+  background: url('assets/backToTop.svg');
+  background-repeat: no-repeat;
+  background-size: auto;
+  background-position: center;
+  margin: auto;
 `
+
+
