5062b4d694b71712fa2dfc8b53b991ab1f32062c
F-Rigo <f.rigo.dev@gmail.com>
2021-11-24 08:33:58 -0300

    fix: remove icons from partners

--- src/templates/About/Partners/index.tsx
+++ src/templates/About/Partners/index.tsx
@@ -15,8 +15,6 @@ const Partners = () => (
     </S.TitleAndIcon>
     <S.Partners>
       <img src="assets/about-heimdall-logo.svg" alt="Partner Logo" />
-      <img src="assets/about-lemonade-logo.svg" alt="Partner Logo" />
-      <img src="assets/about-jigstack-logo.svg" alt="Partner Logo" />
       <img src="assets/about-api3-logo.svg" alt="Partner Logo" />
     </S.Partners>
   </S.Container>
