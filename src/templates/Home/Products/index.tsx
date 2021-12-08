22495797f873930b76ef4e4bbb823e4fa16e1670
André Zanghelini <An-dz@users.noreply.github.com>
2021-12-08 14:11:42 -0300

    fix: rename properties React was complaining about

--- src/templates/Home/Products/index.tsx
+++ src/templates/Home/Products/index.tsx
@@ -29,9 +29,9 @@ const Products = () => {
         <S.Link href='https://drive.google.com/file/d/12jxIMtBVqaY7bMbLmt52Lo0xDFUURZsb/view?usp=sharing' target='_blank' rel='noopener noreferral'>
           <span>Read the Whitepaper</span>
           <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" >
-            <path d="M8.5 16C12.6421 16 16 12.6421 16 8.5C16 4.35786 12.6421 1 8.5 1C4.35786 1 1 4.35786 1 8.5C1 12.6421 4.35786 16 8.5 16Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M8.5 11.5L11.5 8.5L8.5 5.5" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M5.5 8.5H11.5" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8.5 16C12.6421 16 16 12.6421 16 8.5C16 4.35786 12.6421 1 8.5 1C4.35786 1 1 4.35786 1 8.5C1 12.6421 4.35786 16 8.5 16Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8.5 11.5L11.5 8.5L8.5 5.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M5.5 8.5H11.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </S.Link>
         <S.Link>
