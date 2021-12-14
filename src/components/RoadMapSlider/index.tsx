4202519eb8b739895856b26852529bad7a0ca982
Fernando Rigo <f.rigo.dev@gmail.com>
2021-12-14 19:18:23 -0300

    feat: add style and settings and items on roadmapslider component

--- src/components/RoadMapSlider/index.tsx
+++ src/components/RoadMapSlider/index.tsx
@@ -1,24 +1,130 @@
-import Slider from '../../components/Slider'
-import RoadMapCard from '../RoadMapCards'
+import Slider, { SliderSettings } from '../../components/Slider'
+import RoadMapCard from '../RoadMapCard'
 import * as S from './styles'
+import styled from 'styled-components'
 
-const settings = {
-  className: 'center',
-  centerMode: true,
-  infinite: true,
-  centerPadding: '60px',
-  slidesToShow: 2,
-  speed: 500
+const settings: SliderSettings = {
+  arrows: true,
+  slidesToShow: 2.2,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1
+      }
+    }
+  ],
+  nextArrow: <img src="/assets/ArrowNext.svg" alt="" />,
+  prevArrow: <img src="/assets/ArrowPrevious.svg" alt="" />
 }
 
-const RoadMapSlider = () => {
-  return (
-    <S.Wrapper>
-      <Slider settings={settings}>
-        <RoadMapCard />
-      </Slider>
-    </S.Wrapper>
-  )
-}
+const RoadMapSlider = () => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {arrCard.map(card => (
+        <RoadMapCard
+          key={card.title}
+          color={card.color}
+          title={card.title}
+          date={card.date}
+          items={card.items}
+        />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
 
+const arrCard = [
+  {
+    date: '2020-06-01',
+    title: '1st Cycle ',
+    color: '#5EE56B80',
+    items: [
+      {
+        title: 'IDO',
+        text: ['Launching and bootstrapping KACY']
+      },
+      {
+        title: 'Avalanche Social Index',
+        text: [
+          'A single asset that automatically invests in the most active communities on Avalanche'
+        ]
+      },
+      {
+        title: 'Stake & Farm',
+        text: ['Earn staking KACY and other assets']
+      }
+    ]
+  },
+  {
+    date: '2020-06-01',
+    color: '#ffbf008b',
+    title: '2nd Cycle ',
+    items: [
+      {
+        title: 'Governance & DAO',
+        text: ['Vote and take a share of Kassandra profits']
+      },
+      {
+        title: 'Tricrypto Social Index',
+        text: [
+          'Bitcoin, Ethereum, and stablecoins, magically working towards better returns'
+        ]
+      }
+    ]
+  },
+  {
+    date: '2022 - Q2',
+    color: '#21142654',
+    title: '3rd Cycle ',
+    items: [
+      {
+        title: 'Community Products',
+        text: [
+          'Open interface to Kassandra DAO members create their own ',
+          'tokenized funds'
+        ]
+      },
+      {
+        title: 'Multichain',
+        text: ['Time to go to other chains']
+      }
+    ]
+  },
+  {
+    date: '2022 - Q2',
+    color: '#21142654',
+    title: '4th Cycle ',
+    items: [
+      {
+        title: 'Community Products',
+        text: [
+          'Open interface to Kassandra DAO members create their own ',
+          'tokenized funds'
+        ]
+      },
+      {
+        title: 'Multichain',
+        text: ['Time to go to other chains']
+      }
+    ]
+  }
+]
 export default RoadMapSlider
--- src/components/RoadMapSlider/styles.ts
+++ src/components/RoadMapSlider/styles.ts
@@ -1,3 +1,62 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.section``
+export const Wrapper = styled.section`
+  max-width: 92rem;
+  margin: 0 auto;
+  margin-bottom: 15rem;
+  ${media.lessThan('large')`
+      overflow-x: hidden;
+      padding: 0 3.2rem;
+    `}
+
+  .slick-track,
+    .slick-list {
+    display: flex;
+  }
+
+  .slick-slide > div {
+    margin: 0 0.8rem;
+    flex: 1 0 auto;
+    height: 100%;
+  }
+
+  .slick-list {
+    margin: 0 -0.8rem;
+  }
+
+  ${media.greaterThan('medium')`
+      .slick-slide > div {
+        margin: 0 1.6rem;
+      }
+
+      .slick-list {
+        margin: 0 -1.6rem;
+      }
+    `}
+
+  .slick-prev,
+    .slick-next {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0;
+    transform: translate(0, -50%);
+  }
+
+  .slick-prev {
+    left: -5.6rem;
+  }
+
+  .slick-next {
+    right: -5.6rem;
+  }
+
+  .slick-prev.slick-disabled,
+  .slick-next.slick-disabled {
+    visibility: hidden;
+  }
+`
