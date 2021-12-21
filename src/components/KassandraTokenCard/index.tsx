aa0ea60ae44253530cb00a2a47708203d3e5d85a
jony <jonyreiscardoso@gmail.com>
2021-12-21 10:44:14 -0400

    style: update logo image on KassandraTokenCard component

--- src/components/KassandraTokenCard/index.tsx
+++ src/components/KassandraTokenCard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import * as S from './styles'
 
-const KassandraToken = () => {
+const KassandraTokenCard = () => {
   return (
     <S.Container>
       <S.KassandraToken>
@@ -86,7 +86,73 @@ const KassandraToken = () => {
         <S.Card>
           <S.KasasndraCardHeader>
             <S.ImageWrapper>
-              <img src="assets/logo-64.svg" alt="" />
+              <svg
+                width="100"
+                height="94"
+                viewBox="0 0 100 94"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g filter="url(#filter0_f_7445_10921)">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M49.9909 21.1245C34.0376 21.1247 21.1091 34.2359 21.1091 50.4149C21.1091 59.237 24.9514 67.1477 31.0392 72.5265L44.7738 60.7852H30.8776C29.4407 60.7852 28.2972 59.6012 28.2972 58.1684C28.2972 56.7115 29.4646 55.5517 30.8776 55.5517H69.1224C70.5593 55.5517 71.7028 56.7357 71.7028 58.1684C71.7028 59.6254 70.5354 60.7852 69.1224 60.7852H56.873L69.5223 72.0297C75.276 66.6764 78.8909 58.9837 78.8909 50.4328C78.8909 34.2543 65.9451 21.1422 49.9909 21.1245ZM20 50.4149C20 33.615 33.4251 20 49.9912 20H49.9918C66.5569 20.0182 80 33.6323 80 50.4328C80 59.4973 76.0866 67.6339 69.8955 73.2027L69.5297 73.5318L53.9257 59.6607H69.1224C69.929 59.6607 70.5937 58.9982 70.5937 58.1684C70.5937 57.3501 69.9403 56.6762 69.1224 56.6762H30.8776C30.071 56.6762 29.4063 57.3386 29.4063 58.1684C29.4063 58.9867 30.0597 59.6607 30.8776 59.6607H47.8098L31.0361 74L30.6786 73.6943C24.1468 68.1074 20 59.7525 20 50.4149Z"
+                    fill="url(#paint0_linear_7445_10921)"
+                  />
+                </g>
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M49.9909 20.9039C33.9169 20.9041 20.8905 34.1293 20.8905 50.4488C20.8905 59.3824 24.7924 67.3895 30.9677 72.8159L45.0592 60.7562H30.8067C29.4265 60.7562 28.328 59.6177 28.328 58.2398C28.328 56.8388 29.4493 55.7234 30.8067 55.7234H69.1933C70.5735 55.7234 71.672 56.8619 71.672 58.2398C71.672 59.6407 70.5508 60.7562 69.1933 60.7562H56.6027L69.5963 72.3197C75.4362 66.9173 79.1095 59.1285 79.1095 50.4668C79.1095 34.1477 66.0657 20.9218 49.9909 20.9039ZM20 50.4488C20 33.6301 33.4251 20 49.9912 20H49.9916C66.5569 20.0182 80 33.6476 80 50.4668C80 59.5414 76.0867 67.687 69.8955 73.2622L69.6017 73.5267L54.2361 59.8522H69.1933C70.0638 59.8522 70.7815 59.1366 70.7815 58.2398C70.7815 57.3558 70.0764 56.6273 69.1933 56.6273H30.8067C29.9362 56.6273 29.2186 57.343 29.2186 58.2398C29.2186 59.1238 29.9236 59.8522 30.8067 59.8522H47.4969L30.9657 74L30.6787 73.7542C24.1466 68.1609 20 59.7968 20 50.4488Z"
+                  fill="url(#paint1_linear_7445_10921)"
+                />
+                <defs>
+                  <filter
+                    id="filter0_f_7445_10921"
+                    x="0"
+                    y="0"
+                    width="100"
+                    height="94"
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                  >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="BackgroundImageFix"
+                      result="shape"
+                    />
+                    <feGaussianBlur
+                      stdDeviation="10"
+                      result="effect1_foregroundBlur_7445_10921"
+                    />
+                  </filter>
+                  <linearGradient
+                    id="paint0_linear_7445_10921"
+                    x1="50.0461"
+                    y1="-3.87328"
+                    x2="50.0461"
+                    y2="91.7196"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#FFBF00" />
+                    <stop offset="1" stopColor="#E843C4" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_7445_10921"
+                    x1="50"
+                    y1="20.452"
+                    x2="50"
+                    y2="73.4088"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#FFBF00" />
+                    <stop offset="1" stopColor="#E843C4" />
+                  </linearGradient>
+                </defs>
+              </svg>
             </S.ImageWrapper>
           </S.KasasndraCardHeader>
           <S.TextWrapper>
@@ -256,4 +322,4 @@ const KassandraToken = () => {
   )
 }
 
-export default KassandraToken
+export default KassandraTokenCard
