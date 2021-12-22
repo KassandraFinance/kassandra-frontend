35a5b49d231c9ee79709e70098717a84b35ebf2a
André Zanghelini <An-dz@users.noreply.github.com>
2021-12-22 15:46:24 -0300

    chg: addresses to Avalanche Fuji Testnet

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,12 +1,12 @@
-export const HeimCRPPOOL = '0xEe3F5a6915a93c27E10746c372d59f5A485955bf'
-export const HeimCorePool = '0x5c3DE8BB50F5d81d506422BEb1DacbF628b019Ff'
+export const HeimCRPPOOL = '0xE34A2935B04e9c879f5bDd022b97D7Cf2F1Dde1d'
+export const HeimCorePool = '0xFCfB171A8a4666bc53Eac2d91Cb0b5203DDa63cD'
 
 export const LPKacyAvax = '0xbaa8b0d2AA6415d5b4077C1FA06b3507577FBCd7'
 export const LPDaiAvax = '0xe64b1772a9e28E694FEc27Bc7901f88855252E9F'
 export const Kacy = '0x1d7C6846F033e593b4f3f21C39573bb1b41D43Cb'
 export const Staking = '0xe08eD1e470160AF3dF917be221a6aed6284c1D2F'
-export const Timelock = '0x22bc71B8673193559f7af6096FabF6a2d046CFC7'
-export const GovernorAlpha = '0x52f47a34120B114Cd4FE265B710417B297D76384'
+export const Timelock = ''
+export const GovernorAlpha = ''
 
 export const SUBGRAPH_URL =
   'https://graph.kassandra.finance/subgraphs/name/Kassandra'
