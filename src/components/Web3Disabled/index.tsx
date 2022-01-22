96d99aff5f1e203fe2247a391ddb2d3db895db97
jonyreis <jonyreiscardoso@gmail.com>
2022-01-22 13:57:45 -0400

    feat: add mainnet logic

--- src/components/Web3Disabled/index.tsx
+++ src/components/Web3Disabled/index.tsx
@@ -27,7 +27,7 @@ const Web3Disabled = ({ textButton, textHeader, bodyText, type }: IWeb3DisabledP
         setOpenWallet(true)
         break;
       case "changeChain":
-        changeChain(chains.fuji)
+        changeChain(process.env.MASTER === '1' ? chains.avalanche : chains.fuji)
         break;
       default:
         break;
