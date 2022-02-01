25ccc4941f481d0f4aeebea216585fad8628e978
F-Rigo <f.rigo.dev@gmail.com>
2022-02-01 10:27:18 -0300

    fix: change substr utils to reduce wallet endress length

--- src/utils/substr.ts
+++ src/utils/substr.ts
@@ -1,5 +1,5 @@
 function substr(address: string) {
-  return address.substr(0, 6) + "...." + address.substr((address.length - 4), address.length)
+  return address.substr(0, 4) + "...." + address.substr((address.length - 4), address.length)
 }
 
 export default substr
