7b0ed87f9c72598ffd477494796e2abd898a7123
jony <jonyreiscardoso@gmail.com>
2022-02-10 21:24:35 -0400

    refactor: update ModalManageVotingPower

--- src/components/Governance/ModalManageVotingPower/index.tsx
+++ src/components/Governance/ModalManageVotingPower/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 // import { useMatomo } from '@datapunt/matomo-tracker-react'
 
+import ManageVotingPower from './ManageVotingPower'
+import DelegateVotingPower from './DelegateVotingPower'
+import UndelegateVotingPower from './UndelegateVotingPower'
+
 import * as S from './styles'
 
 interface IModalManageVotingPowerProps {
@@ -12,6 +16,7 @@ const ModalManageVotingPower = ({
   modalOpen,
   setModalOpen
 }: IModalManageVotingPowerProps) => {
+  const [currentModal, setCurrentModal] = React.useState<string>('manage')
   // const { trackEvent } = useMatomo()
 
   // function matomoEvent(action: string, name: string) {
@@ -25,54 +30,36 @@ const ModalManageVotingPower = ({
   return (
     <>
       <S.Backdrop
-        onClick={() => setModalOpen(false)}
+        onClick={() => {
+          setModalOpen(false)
+          setCurrentModal('manage')
+        }}
         style={{ display: modalOpen ? 'block' : 'none' }}
       />
       <S.ModalContainer modalOpen={modalOpen}>
-        <S.Top>
-          <p>Manage your voting power</p>
-          <S.Close type="button" onClick={() => setModalOpen(false)}>
+        <S.Header>
+          {currentModal === 'manage' && <p>Manage your voting power</p>}
+          {currentModal === 'delegate' && <p>Delegate voting power</p>}
+          {currentModal === 'undelegate' && <p>Undelegate voting power</p>}
+          <S.Close
+            type="button"
+            onClick={() => {
+              setModalOpen(false)
+              setCurrentModal('manage')
+            }}
+          >
             <img src="/assets/close.svg" alt="Close" />
           </S.Close>
-        </S.Top>
-        <S.Content>
-          <S.WrapperIconsBackGround
-            type="button"
-            onClick={() => {
-              setModalOpen(false)
-            }}
-          >
-            <S.WrapperIcons>
-              <img src="/assets/icons/delegate.svg" alt="" />
-              <S.Option>
-                <h3>Delegate your voting power</h3>
-                <p>
-                  This option allows you to delegate your voting power to
-                  another Avalanche address. Your KACY tokens will{' '}
-                  <strong>not</strong> be sent, only your voting rights. This
-                  action can be undone at any time.
-                </p>
-              </S.Option>
-            </S.WrapperIcons>
-          </S.WrapperIconsBackGround>
-          <S.WrapperIconsBackGround
-            type="button"
-            onClick={() => {
-              setModalOpen(false)
-            }}
-          >
-            <S.WrapperIcons>
-              <img src="/assets/icons/undelegate.svg" alt="" />
-              <S.Option>
-                <h3>Undelegate your voting power</h3>
-                <p>
-                  This option allows you to undelegate the voting power you have
-                  sent to another Avalanche address.
-                </p>
-              </S.Option>
-            </S.WrapperIcons>
-          </S.WrapperIconsBackGround>
-        </S.Content>
+        </S.Header>
+        {currentModal === 'manage' && (
+          <ManageVotingPower setCurrentModal={setCurrentModal} />
+        )}
+        {currentModal === 'delegate' && (
+          <DelegateVotingPower setCurrentModal={setCurrentModal} />
+        )}
+        {currentModal === 'undelegate' && (
+          <UndelegateVotingPower setCurrentModal={setCurrentModal} />
+        )}
       </S.ModalContainer>
     </>
   )
--- src/components/Governance/ModalManageVotingPower/styles.ts
+++ src/components/Governance/ModalManageVotingPower/styles.ts
@@ -43,7 +43,7 @@ export const ModalContainer = styled.div<IModalContainerProps>`
   }
 `
 
-export const Top = styled.div`
+export const Header = styled.div`
   background-color: #1f1f1fb8;
   border-bottom: 1px solid #ffffff40;
   border-radius: 10px 10px 0 0;
@@ -79,103 +79,4 @@ export const Close = styled.button`
   padding: 2px;
 
   cursor: pointer;
-`
-
-export const Content = styled.div`
-  background: #1f2937;
-  border-radius: 0 0 10px 10px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`
-
-export const WrapperIconsBackGround = styled.button`
-  background: #1f1f1fb8;
-  border-radius: 1rem;
-  border: none;
-
-  display: flex;
-  align-items: center;
-
-  margin-top: 2.4rem;
-  margin-right: 2.4rem;
-  margin-left: 2.4rem;
-  padding: 0.1rem;
-
-  @media (max-width: 440px) {
-    margin-top: 1.6rem;
-    margin-right: 1.6rem;
-    margin-left: 1.6rem;
-  }
-
-  &:last-child {
-    margin-top: 1.6rem;
-    margin-bottom: 2.4rem;
-  }
-
-  &.disabled {
-    filter: grayscale(1);
-  }
-
-  &:hover,
-  &:focus {
-    background: linear-gradient(0deg, #ffbf00 -0.02%, #e843c4 99.99%);
-  }
-
-  &:focus {
-    outline: none;
-  }
-`
-
-export const WrapperIcons = styled.div`
-  background: #1f1f1f;
-  border: none;
-  border-radius: 1rem;
-
-  display: flex;
-  align-items: center;
-
-  padding-top: 1.6rem;
-  padding-right: 1.6rem;
-  padding-bottom: 1.6rem;
-
-  cursor: pointer;
-  position: relative;
-  z-index: 2;
-
-  @media (max-width: 440px) {
-    padding-left: 1.6rem;
-  }
-
-  img {
-    margin: 0 1.6rem;
-    @media (max-width: 440px) {
-      display: none;
-    }
-  }
-`
-
-export const Option = styled.div`
-  color: ${theme.colors.snow};
-  font-family: ${theme.font.family};
-  text-align: left;
-  letter-spacing: 0.05rem;
-
-  h3 {
-    font-size: ${theme.font.sizes.font14};
-    font-weight: ${theme.font.weight.bold};
-
-    margin-bottom: ${theme.spacings.space8};
-  }
-  p {
-    line-height: ${theme.font.sizes.font18};
-    font-size: ${theme.font.sizes.font14};
-    font-weight: ${theme.font.weight.light};
-
-    @media (max-width: 440px) {
-      font-size: ${theme.font.sizes.font12};
-    }
-  }
 `
