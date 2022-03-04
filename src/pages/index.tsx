8a9f0cbd788ec6715206a5d58b0a4a76810bbe13
jony <jonyreiscardoso@gmail.com>
2022-03-04 11:29:04 -0400

    refactor: update overview api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
+import { SWRConfig } from 'swr'
+
 import Home from '../templates/Home'
 
 export default function Index() {
-  return <Home />
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 10000,
+        fetcher: url => fetch(url).then(res => res.json())
+      }}
+    >
+      <Home />
+    </SWRConfig>
+  )
 }
