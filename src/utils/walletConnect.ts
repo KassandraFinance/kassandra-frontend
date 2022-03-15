3b82dc28c1453db509adb9c729cb06ce5c4ec313
Edmar Alencar <edmaaralencar1@gmail.com>
2022-03-15 10:03:17 -0300

    fix: fix wallet connect configs

--- src/utils/walletConnect.ts
+++ src/utils/walletConnect.ts
@@ -1,8 +1,9 @@
 import WalletConnect from '@walletconnect/client'
+import { ToastError, ToastSuccess } from '../components/Toastify/toast'
 
 export async function subscribeToEvents(
-  connector: WalletConnect, 
-  handleAccountsChanged: { (accounts: any): Promise<void>; (arg0: any): void }
+  connector: WalletConnect,
+  handleAccountsChanged: (accounts: any) => void
 ) {
   if (!connector) {
     return
@@ -19,6 +20,7 @@ export async function subscribeToEvents(
       // Get provided accounts and chainId
       const { accounts, chainId } = payload.params[0]
       handleAccountsChanged(accounts)
+      ToastSuccess('Connected to Wallet Connect.')
     }
   )
 
@@ -39,7 +41,8 @@ export async function subscribeToEvents(
     if (error) {
       throw error
     }
-    // Delete connector
+
+    window.location.reload()
   })
 
   connector.on(
