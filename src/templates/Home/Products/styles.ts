e1cd4157a10a92b8729c8d8e503f4c1ae2249cfb
gui c <guilhermescorrea@hotmail.com.br>
2022-03-21 11:04:22 -0300

    refactor: organize order declaration css

--- src/templates/Home/Products/styles.ts
+++ src/templates/Home/Products/styles.ts
@@ -2,22 +2,21 @@ import styled from 'styled-components'
 import theme from '../../../styles/theme'
 
 export const Products = styled.section`
-  margin: 0 auto 160px;
-  max-width: 960px;
+  display: grid;
+  gap: 10rem;
+  grid-template-columns: 1fr 1fr;
+  max-width: 96rem;
+  margin: 0 auto 16rem;
   padding: 0 ${theme.spacings.space32};
 
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 100px;
-
   @media (max-width: 960px) {
-    margin: 0 auto 80px;
-
     display: grid;
-    gap: 50px;
+    gap: 5rem;
     grid-template-columns: 1fr;
+    margin: 0 auto 8rem;
   }
 `
+
 export const TextWrapper = styled.div`
   h1 {
     margin-bottom: ${theme.spacings.space32};
@@ -26,16 +25,20 @@ export const TextWrapper = styled.div`
     font-weight: ${theme.font.weight.black};
     line-height: 104%;
     text-align: left;
+
     @media (max-width: 960px) {
       font-size: ${theme.font.sizes.font36};
     }
+
     @media (max-width: 760px) {
       font-size: ${theme.font.sizes.font32};
     }
+
     @media (max-width: 450px) {
       font-size: ${theme.font.sizes.font24};
     }
   }
+
   p {
     margin: 0 auto;
     margin-bottom: ${theme.spacings.space24};
@@ -44,49 +47,60 @@ export const TextWrapper = styled.div`
     line-height: 155%;
     color: ${theme.colors.amber};
     text-align: left;
-    letter-spacing: 5.6px;
+    letter-spacing: 0.56rem;
+
     @media (max-width: 450px) {
       font-size: ${theme.font.sizes.font12};
     }
   }
+
   span {
     font-size: ${theme.font.sizes.font16};
     font-weight: ${theme.font.weight.light};
     line-height: 155%;
     text-align: left;
+
     @media (max-width: 760px) {
       text-align: center;
     }
+
     @media (max-width: 450px) {
       font-size: ${theme.font.sizes.font14};
     }
   }
 `
+
 export const Divider = styled.div`
-  max-width: 100px;
-  margin-block: 40px;
+  max-width: 10rem;
+  margin-block: 4rem;
 
-  border-bottom: 3px solid #ffbf00;
-  border-radius: 4px;
+  border-bottom: 0.3rem;
+  border-bottom-style: solid;
+  border-bottom-color: #ffbf00;
+  border-radius: 0.4rem;
 `
-export const Link = styled.a`
-  margin-bottom: 20px;
-  max-width: max-content;
 
+export const Link = styled.a`
   display: flex;
   align-items: center;
+  margin-bottom: 2rem;
+  max-width: max-content;
 
   text-decoration: none;
-  cursor: pointer;
   color: ${theme.colors.snow};
+
   transition: 0.15s;
+  cursor: pointer;
+
   span {
     display: flex;
-    margin-right: 20px;
+    margin-right: 2rem;
   }
+
   svg {
     margin-left: ${theme.spacings.space8};
   }
+
   &:hover {
     color: ${theme.colors.cyan};
     > svg {
@@ -96,59 +110,68 @@ export const Link = styled.a`
     }
   }
 `
+
 export const CardContainer = styled.div`
   display: grid;
+  column-gap: 3.2rem;
   grid-template-columns: 1fr 1fr;
   justify-content: center;
-  column-gap: 32px;
+
   @media (max-width: 960px) {
-    row-gap: 24px;
+    row-gap: 2.4rem;
   }
+
   p {
-    margin-top: 26px;
-    margin-bottom: 12px;
+    margin-top: 2.6rem;
+    margin-bottom: 1.2rem;
 
     font-size: ${theme.font.sizes.font14};
     color: ${theme.colors.amber};
     text-align: left;
-    letter-spacing: 5.6px;
-    letter-spacing: 4px;
+    letter-spacing: 0.56rem;
+    letter-spacing: 0.4rem;
+
     @media (max-width: 450px) {
       font-size: ${theme.font.sizes.font12};
-      letter-spacing: 0px;
+      letter-spacing: 0;
     }
   }
+
   span {
     font-size: ${theme.font.sizes.font14};
     font-weight: ${theme.font.weight.light};
     line-height: 122.5%;
     text-align: left;
+
     @media (max-width: 450px) {
       font-size: ${theme.font.sizes.font14};
       line-height: 100%;
     }
   }
 `
+
 export const Card = styled.div`
   margin: 0 auto;
+
   @media (max-width: 800px) {
-    max-width: 250px;
+    max-width: 25rem;
   }
+
   @media (max-width: 450px) {
-    max-width: 130px;
+    max-width: 13rem;
   }
 `
+
 export const IconWrapper = styled.div`
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 12px;
-
-  margin-bottom: 26px;
-  max-width: auto;
-  margin: auto;
-  width: 78px;
-  height: 73px;
-
   display: table-cell;
   vertical-align: middle;
   text-align: center;
+  margin-bottom: 2.6rem;
+  max-width: auto;
+  width: 7.8rem;
+  height: 7.3rem;
+  margin: auto;
+
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 1.2rem;
 `
