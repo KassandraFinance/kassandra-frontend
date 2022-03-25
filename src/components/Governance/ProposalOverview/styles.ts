f7e38c844ac113200a151d2998071d77b68b7d9d
ChristianDaniel <christian_daniel04@hotmail.com>
2022-03-25 15:10:38 -0300

    feat: consume smart contract data and develop proposal overview component logic

--- src/components/Governance/ProposalOverview/styles.ts
+++ src/components/Governance/ProposalOverview/styles.ts
@@ -2,9 +2,7 @@ import styled from 'styled-components'
 import theme from '../../../styles/theme'
 
 export const ProposalOverview = styled.div`
-  background: rgba(255, 255, 255, 0.04);
-  border: 1px solid rgba(255, 255, 255, 0.15);
-  border-radius: 20px;
+  position: relative;
 
   display: flex;
   justify-content: space-between;
@@ -13,88 +11,161 @@ export const ProposalOverview = styled.div`
   max-width: 44rem;
   padding: ${theme.spacings.space24};
 
-  position: relative;
+  background-color: rgba(255, 255, 255, 0.04);
+  border-radius: 2rem;
+  border: 0.1rem solid rgba(255, 255, 255, 0.15);
 
   @media (max-width: 420px) {
-    padding: ${theme.spacings.space16};
     width: 100%;
+    padding: ${theme.spacings.space16};
+  }
+`
+
+export const ProposalOverviewIsLoading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 39rem;
+  height: 13rem;
+  margin-left: 2rem;
+
+  @media (max-width: 960px) {
+    width: 30rem;
+    height: 12.8rem;
+  }
+
+  div {
+    margin-top: 0;
   }
 `
 
 export const Status = styled.ul`
+  width: 21.2rem;
   margin-right: ${theme.spacings.space56};
 
-  width: 212px;
-
   @media (max-width: 960px) {
+    width: 16rem;
     margin-right: ${theme.spacings.space32};
-
-    width: 160px;
   }
 
   h3 {
-    line-height: ${theme.font.sizes.font16};
+    margin-bottom: 0.4rem;
+
     font-size: ${theme.font.sizes.font16};
+    line-height: ${theme.font.sizes.font16};
     font-weight: ${theme.font.weight.bold};
 
-    margin-bottom: 4px;
     @media (max-width: 960px) {
-      line-height: ${theme.font.sizes.font14};
       font-size: ${theme.font.sizes.font14};
+      line-height: ${theme.font.sizes.font14};
     }
   }
-  li {
-    border-bottom: 1px solid ${theme.colors.grayDisabled};
-    color: ${theme.colors.grayDisabled};
-    line-height: ${theme.font.sizes.font16};
-    font-size: ${theme.font.sizes.font16};
-    font-weight: ${theme.font.weight.light};
 
-    display: flex;
-    justify-content: space-between;
-
-    padding: 12px 0;
-
-    @media (max-width: 960px) {
-      line-height: ${theme.font.sizes.font14};
-      font-size: ${theme.font.sizes.font14};
+  animation: isLoading 950ms ease;
+  @keyframes isLoading {
+    from {
+      opacity: 0;
     }
-
-    span {
-      &:first-child {
-        &:before {
-          content: '';
-          display: inline-block;
-          background-color: ${theme.colors.green};
-          border-radius: 3px;
-
-          width: ${theme.spacings.space16};
-          height: ${theme.spacings.space16};
-          margin-right: ${theme.spacings.space16};
-        }
-      }
-    }
-
-    &:last-child {
-      border: none;
-      padding-top: 12px;
-      padding-bottom: 0;
+    to {
+      opacity: 1;
     }
   }
 `
 
+interface IProposalStateProps {
+  ProposalState: string;
+}
+
+// eslint-disable-next-line prettier/prettier
+export const ProposalStateList = styled.li<IProposalStateProps>`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.2rem 0;
+
+  color: ${theme.colors.grayDisabled};
+  font-size: ${theme.font.sizes.font16};
+  line-height: ${theme.font.sizes.font16};
+  font-weight: ${theme.font.weight.light};
+
+  border-bottom: 0.1rem solid ${theme.colors.grayDisabled};
+
+  @media (max-width: 960px) {
+    font-size: ${theme.font.sizes.font14};
+    line-height: ${theme.font.sizes.font14};
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    margin-left: 0.5rem;
+
+    &:first-child {
+      &:before {
+        content: '';
+        display: inline-block;
+
+        width: ${theme.spacings.space16};
+        height: ${theme.spacings.space16};
+        margin-right: ${theme.spacings.space16};
+
+        border-radius: 0.3rem;
+        background-color: ${props => {
+          switch (props.ProposalState) {
+            case 'Failed':
+              return theme.colors.red
+            case 'Active':
+              return theme.colors.magenta
+            case 'Succeeded':
+              return theme.colors.green
+            default:
+              return 'white'
+          }
+        }};
+      }
+    }
+  }
+
+  &:last-child {
+    padding-top: 1.2rem;
+    padding-bottom: 0;
+    border: none;
+  }
+`
+
+export const ProposalOverviewGraphic = styled.div`
+  @media (max-width: 320px) {
+    margin-top: 3rem;
+    margin-left: -1rem;
+  }
+
+  @media (max-width: 300px) {
+    margin-left: -3rem;
+  }
+`
+
 export const Total = styled.div`
+  position: absolute;
+  top: 7rem;
+  right: 7rem;
+
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  position: absolute;
-  top: 70px;
-  right: 72px;
-
   @media (max-width: 960px) {
-    top: 60px;
-    right: 62px;
+    top: 7.2rem;
+    right: 7.2rem;
+  }
+
+  @media (max-width: 420px) {
+    top: 6.5rem;
+    right: 6.3rem;
+  }
+
+  @media (max-width: 320px) {
+    top: 9.5rem;
+    right: 5rem;
   }
 
   .total {
