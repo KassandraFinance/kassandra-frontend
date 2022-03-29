211bd0fb94247ba81b18eaa46f961bbd236e538b
gui c <guilhermescorrea@hotmail.com.br>
2022-03-29 12:04:19 -0300

    refactor: order elements css

--- src/components/Governance/VoteCard/styles.ts
+++ src/components/Governance/VoteCard/styles.ts
@@ -4,16 +4,15 @@ import theme from '../../../styles/theme'
 import * as ButtonStyles from '../../Button/styles'
 
 export const Container = styled.div`
-  background: #1f1f1fb8;
-  border-radius: 1rem;
-
   display: flex;
   flex-direction: column;
-
   padding: 2.4rem;
   width: 100%;
   max-width: 55rem;
   height: min-content;
+
+  background: #1f1f1fb8;
+  border-radius: 1rem;
 
   @media (max-width: 670px) {
     max-width: 100%;
@@ -31,46 +30,53 @@ export const TotalPercentage = styled.h3`
 `
 export const TotalVotes = styled.h3`
   font-size: ${theme.font.sizes.font18};
-  letter-spacing: 0.9px;
+  letter-spacing: 0.09rem;
   font-weight: ${theme.font.weight.light};
   color: ${theme.colors.snow};
 `
 
 export const VoteBar = styled.div`
-  background-color: #8b8b8b;
-  border-radius: 0.1rem;
   display: block;
-
   width: 100%;
   height: 0.4rem;
   margin-bottom: 4rem;
+
+  background-color: #8b8b8b;
+  border-radius: 0.1rem;
 `
 
 export const ActionWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   ${ButtonStyles.Wrapper} {
     width: min(27.3rem, 50%);
   }
 `
 export const Link = styled.a`
-  text-decoration: none;
   display: flex;
+
+  text-decoration: none;
+  color: ${theme.colors.snow};
+
   cursor: pointer;
   transition: 0.15s;
-  color: ${theme.colors.snow};
+
   span {
+    display: flex;
+    margin-right: 2rem;
+
     font-size: ${theme.font.sizes.font14};
     letter-spacing: 0;
-    display: flex;
-    margin-right: 20px;
   }
   svg {
     margin-left: ${theme.spacings.space8};
   }
+
   &:hover {
     color: ${theme.colors.cyan};
+
     > svg {
       path {
         stroke: ${theme.colors.cyan};
