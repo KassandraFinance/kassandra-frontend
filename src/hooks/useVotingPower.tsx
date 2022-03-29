09cda88e353364e39d4cfe17a38bece0d475c6bc
Edmar Alencar <edmaaralencar1@gmail.com>
2022-03-29 11:50:25 -0300

    feat: add delegateVote function

--- src/hooks/useVotingPower.tsx
+++ src/hooks/useVotingPower.tsx
@@ -5,9 +5,13 @@ import { AbiItem } from "web3-utils"
 
 import web3 from '../utils/web3'
 import StakingContract from "../constants/abi/Staking.json"
+import { RootStateOrAny, useSelector } from 'react-redux'
+
+import { TransactionCallback } from '../utils/txWait'
 
 
 const useVotingPower = (address: string) => {
+  const { userWalletAddress } = useSelector((state: RootStateOrAny) => state)
   const [contract, setContract] = React.useState(new web3.eth.Contract((StakingContract as unknown) as AbiItem, address))
 
   React.useEffect(() => {
@@ -25,9 +29,18 @@ const useVotingPower = (address: string) => {
       return new BigNumber(value)
     }
 
+    const delegateVote = async (pid: number, address: string, callback: TransactionCallback) => {
+      const test =  await contract.methods.delegate(pid, address)
+        .send({ from: userWalletAddress },
+          callback
+        )
+      console.log(test)
+    }
+
     return {
       currentVotes,
-      totalVotes
+      totalVotes,
+      delegateVote
     }
   }, [contract])
 }
