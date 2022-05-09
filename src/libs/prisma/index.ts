ffa88e5ee932da6b4b59e33eef5e665233377bf4
Edmar Alencar <edmaaralencar1@gmail.com>
2022-05-09 19:40:13 -0300

    fix: fix prisma error

--- src/libs/prisma/index.ts
+++ src/libs/prisma/index.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+// eslint-disable-next-line prettier/prettier
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
 export default prisma
