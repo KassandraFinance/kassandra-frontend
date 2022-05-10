79ba28c1ac49186db1534028432cf2dd4dd2e1c0
Tiago138 <tgnvsctn@gmail.com>
2022-05-10 10:02:34 -0300

    feat: moved PortfolioHeading component to the portfolio component

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import Breadcrumb from '../../components/Breadcrumb'
 import BreadcrumbItem from '../../components/Breadcrumb/BreadcrumbItem'
-import PortfolioHeading from '../../components/PortfolioHeading'
-
-import AssetsIcon from '../../../public/assets/iconGradient/section-title-Assets.svg'
-import StakedPoolsIcon from '../../../public/assets/iconGradient/section-title-Staked-pools.svg'
+import Portfolio from '../../components/Portfolio'
 
 import * as S from './styles'
 
@@ -21,20 +18,7 @@ const Profile = () => {
         </BreadcrumbItem>
       </Breadcrumb>
 
-      <S.Profile>
-        <PortfolioHeading
-          image={AssetsIcon}
-          title="Assets"
-          usd={'16,000,000.00'}
-          change={0.11}
-        />
-        <PortfolioHeading
-          image={StakedPoolsIcon}
-          title="Staking and Farming"
-          usd={'16,000,000.00'}
-          change={-0.11}
-        />
-      </S.Profile>
+      <Portfolio />
     </S.BackgroundProfile>
   )
 }
--- src/templates/Profile/styles.ts
+++ src/templates/Profile/styles.ts
@@ -17,18 +17,3 @@ export const BackgroundProfile = styled.div<IBackgroundProductsProps>`
       ? ''
       : 'box-shadow: inset 0px -20px 20px 0px #151117; padding: 0 0 80px;'};
 `
-
-export const Profile = styled.section`
-  margin: 0 auto;
-  max-width: 1140px;
-  max-height: 100%;
-  margin-top: ${theme.spacings.space32};
-
-  @media (max-width: 1200px) {
-    padding: 0 3rem;
-  }
-
-  @media (max-width: 960px) {
-    padding: 0 2.4rem;
-  }
-`
