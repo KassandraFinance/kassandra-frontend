99ca76530ddd3ea573806b84c2f0e3042dfc45fb
Edmar Alencar <edmaaralencar1@gmail.com>
2022-05-12 21:36:03 -0300

    style: fix responsiviness in share image modal component

--- src/templates/Products/ShareImageModal/styles.ts
+++ src/templates/Products/ShareImageModal/styles.ts
@@ -14,12 +14,20 @@ export const Backdrop = styled.div`
   width: 100vw;
 
   background-color: rgba(0, 0, 0, 0.7);
+
+  @media (max-width: 500px) {
+    align-items: flex-end;
+  }
 `
 
 export const ModalContainer = styled.div`
   border-radius: 1.2rem;
 
   @media (max-width: 1130px) {
+    width: 98.5vw;
+  }
+
+  @media (max-width: 500px) {
     width: 100vw;
   }
 `
@@ -31,10 +39,12 @@ export const ModalHeader = styled.header`
 
   padding: 2.4rem;
 
-  background-color: rgba(31, 31, 31);
-
   font-size: 1.6rem;
   font-weight: 500;
+
+  background-color: rgba(31, 31, 31);
+  border-top-left-radius: 1.2rem;
+  border-top-right-radius: 1.2rem;
 
   .close-modal {
     cursor: pointer;
@@ -42,25 +52,32 @@ export const ModalHeader = styled.header`
 `
 
 export const ModalBody = styled.div`
+  display: flex;
   padding: 2.4rem;
 
   background-color: rgba(31, 41, 55, 1);
+  border-bottom-left-radius: 1.2rem;
+  border-bottom-right-radius: 1.2rem;
 
-  @media (max-width: 1130px) {
-    padding: 0;
-    padding-bottom: 2.4rem;
+  @media (max-width: 500px) {
+    display: block;
+    padding: 3.4rem 2.2rem;
   }
 `
 
 export const SocialMediaContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   flex-wrap: wrap;
+  order: -1;
   gap: 1.6rem;
-  padding: 0 2.5rem;
+  margin-right: 3.2rem;
 
-  @media (max-width: 550px) {
-    gap: 1.6rem 3.6rem;
+  @media (max-width: 500px) {
+    flex-direction: row;
+    gap: 3.2rem;
+    margin-right: 0;
   }
 `
 
@@ -68,9 +85,8 @@ export const SocialMedia = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 1.6rem;
+  gap: 0.8rem;
 
-  margin-top: 2.4rem;
   min-width: 5.6rem;
 
   font-size: 1.2rem;
@@ -88,12 +104,33 @@ export const SocialMedia = styled.div`
     max-width: 4.2rem;
     min-width: 0;
   }
+
+  @media (max-width: 500px) {
+    max-width: 5.6rem;
+    min-width: 0;
+  }
 `
 
 export const ImageContainer = styled.div`
-  @media (max-width: 1020px) {
+  ::-webkit-scrollbar {
+    width: 0.8rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 0.5rem;
+  }
+
+  @media (max-width: 1130px) {
     overflow-x: scroll;
     width: 100%;
-    padding-top: 4rem;
+  }
+
+  @media (max-width: 500px) {
+    opacity: 0;
+    visibility: hidden;
+    width: 0;
+    height: 0;
+    /* display: none; */
   }
 `
