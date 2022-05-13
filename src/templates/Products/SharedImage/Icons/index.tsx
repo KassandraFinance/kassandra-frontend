24ae2268d438fe95642191078680f5914b8922bf
Edmar Alencar <edmaaralencar1@gmail.com>
2022-05-12 21:35:45 -0300

    refactor: refactor styles and icons from shared image component

---
+++ src/templates/Products/SharedImage/Icons/index.tsx
@@ -0,0 +1,391 @@
+import * as S from '../styles'
+
+export const IconBar = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.25 12.9449C0.25 12.6134 0.381696 12.2956 0.616116 12.0612C0.850537 11.8269 1.16848 11.6952 1.5 11.6952H4C4.33152 11.6952 4.64946 11.8269 4.88388 12.0612C5.1183 12.2956 5.25 12.6134 5.25 12.9449V16.6937C5.25 17.0251 5.1183 17.3429 4.88388 17.5773C4.64946 17.8116 4.33152 17.9433 4 17.9433H1.5C1.16848 17.9433 0.850537 17.8116 0.616116 17.5773C0.381696 17.3429 0.25 17.0251 0.25 16.6937V12.9449ZM6.5 7.9464C6.5 7.61499 6.6317 7.29714 6.86612 7.06279C7.10054 6.82845 7.41848 6.69679 7.75 6.69679H10.25C10.5815 6.69679 10.8995 6.82845 11.1339 7.06279C11.3683 7.29714 11.5 7.61499 11.5 7.9464V16.6937C11.5 17.0251 11.3683 17.3429 11.1339 17.5773C10.8995 17.8116 10.5815 17.9433 10.25 17.9433H7.75C7.41848 17.9433 7.10054 17.8116 6.86612 17.5773C6.6317 17.3429 6.5 17.0251 6.5 16.6937V7.9464ZM12.75 1.69834C12.75 1.36692 12.8817 1.04908 13.1161 0.814733C13.3505 0.580386 13.6685 0.44873 14 0.44873H16.5C16.8315 0.44873 17.1495 0.580386 17.3839 0.814733C17.6183 1.04908 17.75 1.36692 17.75 1.69834V16.6937C17.75 17.0251 17.6183 17.3429 17.3839 17.5773C17.1495 17.8116 16.8315 17.9433 16.5 17.9433H14C13.6685 17.9433 13.3505 17.8116 13.1161 17.5773C12.8817 17.3429 12.75 17.0251 12.75 16.6937V1.69834Z"
+        fill="url(#paint0_linear_5582_16287)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_5582_16287"
+          x1="9"
+          y1="0.44873"
+          x2="9"
+          y2="17.9433"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFBF00" />
+          <stop offset="1" stopColor="#E843C4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export const IconUnion = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 19 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.8829 0.746094C10.8821 0.746094 11.8715 0.942902 12.7947 1.32528C13.7178 1.70766 14.5566 2.26813 15.2632 2.97467L9.8829 8.35494L9.8829 0.746094ZM16.0195 5.25558C16.7261 5.96213 17.2865 6.80092 17.6689 7.72407C18.0513 8.64721 18.2481 9.63664 18.2481 10.6358C18.2481 11.6351 18.0513 12.6245 17.6689 13.5476C17.2865 14.4708 16.7261 15.3096 16.0195 16.0161L10.6393 10.6358L16.0195 5.25558ZM10.5715 17.9171C11.7689 17.5538 12.8582 16.9009 13.743 16.0161L8.36275 10.6358L8.36275 3.02701C7.11147 3.02701 5.8795 3.3356 4.77597 3.92545C3.67243 4.5153 2.7314 5.3682 2.03623 6.4086C1.34105 7.44901 0.913193 8.6448 0.790547 9.89005C0.6679 11.1353 0.854252 12.3916 1.3331 13.5476C1.81194 14.7037 2.5685 15.7238 3.53575 16.5176C4.50301 17.3114 5.6511 17.8544 6.87834 18.0985C8.10558 18.3426 9.37408 18.2803 10.5715 17.9171Z"
+        fill="url(#paint0_linear_1972_38246)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1972_38246"
+          x1="9.49889"
+          y1="18.2482"
+          x2="9.49889"
+          y2="0.747873"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFBF00" />
+          <stop offset="1" stopColor="#E843C4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export const IconBirdKassandra = () => {
+  return (
+    <svg
+      width="24"
+      height="20"
+      viewBox="0 0 25 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11.7266 5.32031L11.7789 6.18537L10.9064 6.07944C7.73045 5.6734 4.95586 4.29637 2.60008 1.98367L1.44837 0.836143L1.15172 1.68355C0.523507 3.57255 0.924862 5.56747 2.23363 6.90919C2.93164 7.65067 2.77459 7.75659 1.57052 7.31524C1.15171 7.17401 0.78526 7.06808 0.75036 7.12104C0.628208 7.24462 1.04701 8.85116 1.37857 9.48671C1.83227 10.3694 2.75714 11.2345 3.76925 11.7464L4.62431 12.1525L3.6122 12.1701C2.63498 12.1701 2.60008 12.1878 2.70479 12.5585C3.05379 13.7061 4.43236 14.9242 5.96798 15.4538L7.04989 15.8246L6.10758 16.3895C4.71156 17.2016 3.07124 17.6606 1.43092 17.6959C0.645658 17.7136 0 17.7842 0 17.8371C0 18.0137 2.12893 19.0023 3.36789 19.3907C7.08479 20.5382 11.4997 20.0439 14.8152 18.0843C17.171 16.6896 19.5268 13.9179 20.6262 11.2345C21.2195 9.80448 21.8128 7.19166 21.8128 5.93821C21.8128 5.12612 21.8651 5.02019 22.8423 4.04921C23.4182 3.48427 23.9592 2.86638 24.0639 2.68984C24.2384 2.35441 24.2209 2.35441 23.331 2.65453C21.8477 3.18415 21.6383 3.11354 22.3712 2.3191C22.9121 1.75416 23.5578 0.730218 23.5578 0.430096C23.5578 0.377134 23.2961 0.465405 22.9994 0.624293C22.6853 0.800835 21.9873 1.06565 21.4638 1.22454L20.5215 1.52466L19.6664 0.942068C19.1952 0.624293 18.5321 0.271208 18.1831 0.165283C17.2932 -0.0818758 15.9321 -0.0465674 15.1293 0.2359C12.9481 1.03034 11.5695 3.07823 11.7266 5.32031Z"
+        fill="url(#paint0_linear_657_23659)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_657_23659"
+          x1="12.0724"
+          y1="20.0041"
+          x2="12.0724"
+          y2="0.00203392"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFBF00" />
+          <stop offset="1" stopColor="#E843C4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export const IconUnionKassandra = () => {
+  return (
+    <svg
+      width="24"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.39775 19.8469C8.9668 19.9475 9.55247 20 10.1504 20C10.7466 20 11.3307 19.9478 11.8983 19.8478C14.9617 19.3077 17.5442 17.3732 18.9669 14.7232H19.1543V14.3561C19.7925 13.0393 20.1504 11.5615 20.1504 10C20.1504 8.43854 19.7925 6.96066 19.1543 5.64393V5.27679H18.9669C17.55 2.63745 14.9825 0.707856 11.9353 0.15883C11.3561 0.0544792 10.7596 0 10.1504 0C9.54676 0 8.95562 0.0534848 8.38146 0.15597C5.32726 0.701129 2.7533 2.63285 1.3339 5.27679H1.1543V5.62784C0.511193 6.94862 0.150391 8.43218 0.150391 10C0.150391 11.5678 0.511193 13.0514 1.1543 14.3722V14.7232H1.3339C2.75582 17.3719 5.33642 19.3057 8.39775 19.8469ZM2.24562 6.72125C1.82631 7.73104 1.59485 8.83849 1.59485 10C1.59485 11.1615 1.82631 12.269 2.24562 13.2787H5.09183C4.90666 12.1704 4.82325 11.0171 4.83628 9.87309C4.84838 8.81121 4.94369 7.7462 5.12246 6.72125H2.24562ZM6.93766 5.27679C7.07895 4.79742 7.24229 4.34015 7.42701 3.91174C7.86214 2.90261 8.39735 2.09302 9.01149 1.5196C9.38408 1.47004 9.76425 1.44446 10.1504 1.44446C10.545 1.44446 10.9334 1.47118 11.3139 1.52291C11.9539 2.10181 12.5112 2.91379 12.9647 3.91829C13.1573 4.34486 13.3281 4.79991 13.4762 5.27679H6.93766ZM6.59066 6.72125H13.8427C14.0427 7.71407 14.1549 8.76452 14.1732 9.82445C14.1935 11.0013 14.0978 12.1745 13.8856 13.2787H6.55816C6.36027 12.1957 6.26748 11.0452 6.28064 9.88955C6.29297 8.80768 6.39801 7.73445 6.59066 6.72125ZM15.3137 6.72125C15.498 7.72234 15.5995 8.76167 15.6174 9.79952C15.6376 10.9671 15.5522 12.1458 15.3541 13.2787H18.0552C18.4745 12.269 18.7059 11.1615 18.7059 10C18.7059 8.83849 18.4745 7.73104 18.0552 6.72125H15.3137ZM17.2851 5.27679C16.4048 3.94959 15.164 2.88223 13.7024 2.21434C13.9144 2.56689 14.1073 2.93862 14.2812 3.32394C14.5572 3.93518 14.7908 4.59105 14.9817 5.27679H17.2851ZM5.43786 5.27679H3.01565C3.91378 3.92283 5.18701 2.83929 6.68711 2.17442C6.4706 2.5439 6.27531 2.93464 6.10061 3.33981C5.83911 3.94626 5.61822 4.59679 5.43786 5.27679ZM17.2851 14.7232H15.0321C14.8743 15.2981 14.6838 15.8531 14.4597 16.3797C14.2536 16.864 14.0168 17.3287 13.7482 17.7646C15.1899 17.0954 16.414 16.0365 17.2851 14.7232ZM13.1306 15.814C13.2788 15.4659 13.4112 15.1012 13.5278 14.7232H6.89753C7.0192 15.1398 7.15877 15.5401 7.31617 15.9194C7.76115 16.9919 8.33237 17.8627 9.01665 18.4811C9.38759 18.5302 9.76603 18.5555 10.1504 18.5555C10.5376 18.5555 10.9188 18.5298 11.2924 18.48C12.0451 17.8279 12.6605 16.9185 13.1306 15.814ZM6.64677 17.8076C5.16418 17.1412 3.90571 16.065 3.01565 14.7232H5.39899C5.5584 15.3329 5.75239 15.9196 5.98199 16.473C6.1754 16.9392 6.39656 17.387 6.64677 17.8076Z"
+        fill="url(#paint0_linear_657_23663)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_657_23663"
+          x1="10.148"
+          y1="20.0041"
+          x2="10.148"
+          y2="0.00203392"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFBF00" />
+          <stop offset="1" stopColor="#E843C4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export const Background = () => {
+  return (
+    <S.BackgroundContainer>
+      <svg
+        // width="1440"
+        // height="2030"
+        viewBox="0 0 1440 2030"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMinYMin slice"
+      >
+        <g filter="url(#filter0_f_1462_30422)">
+          <ellipse cx="230.5" cy="210.5" rx="126.5" ry="116.5" fill="#26DBDB" />
+        </g>
+        <g filter="url(#filter1_f_1462_30422)">
+          <ellipse
+            cx="766.5"
+            cy="1020.5"
+            rx="126.5"
+            ry="116.5"
+            fill="#26DBDB"
+          />
+        </g>
+        <g filter="url(#filter2_f_1462_30422)">
+          <ellipse cx="800.5" cy="175.5" rx="127.5" ry="117.5" fill="#FFAC33" />
+        </g>
+        <g filter="url(#filter3_f_1462_30422)">
+          <ellipse
+            cx="1238.5"
+            cy="1137.5"
+            rx="127.5"
+            ry="117.5"
+            fill="#FFAC33"
+          />
+        </g>
+        <g filter="url(#filter4_f_1462_30422)">
+          <ellipse cx="229.5" cy="858.5" rx="127.5" ry="117.5" fill="#FFAC33" />
+        </g>
+        <g filter="url(#filter5_f_1462_30422)">
+          <ellipse
+            cx="1206.5"
+            cy="227.5"
+            rx="127.5"
+            ry="117.5"
+            fill="#E843C4"
+          />
+        </g>
+        <g filter="url(#filter6_f_1462_30422)">
+          <ellipse
+            cx="341.5"
+            cy="1372.5"
+            rx="127.5"
+            ry="117.5"
+            fill="#E843C4"
+          />
+        </g>
+        <g filter="url(#filter7_f_1462_30422)">
+          <ellipse
+            cx="1053.5"
+            cy="672.5"
+            rx="127.5"
+            ry="117.5"
+            fill="#E843C4"
+          />
+        </g>
+        <g filter="url(#filter8_f_1462_30422)">
+          <ellipse cx="626.5" cy="570.5" rx="151.5" ry="139.5" fill="#0C3DDC" />
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_1462_30422"
+            x="-436"
+            y="-446"
+            width="1333"
+            height="1313"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter1_f_1462_30422"
+            x="100"
+            y="364"
+            width="1333"
+            height="1313"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter2_f_1462_30422"
+            x="133"
+            y="-482"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter3_f_1462_30422"
+            x="571"
+            y="480"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter4_f_1462_30422"
+            x="-438"
+            y="201"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter5_f_1462_30422"
+            x="539"
+            y="-430"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter6_f_1462_30422"
+            x="-326"
+            y="715"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter7_f_1462_30422"
+            x="386"
+            y="15"
+            width="1335"
+            height="1315"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+          <filter
+            id="filter8_f_1462_30422"
+            x="-65"
+            y="-109"
+            width="1383"
+            height="1359"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="270"
+              result="effect1_foregroundBlur_1462_30422"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </S.BackgroundContainer>
+  )
+}
--- src/templates/Products/SharedImage/styles.ts
+++ src/templates/Products/SharedImage/styles.ts
@@ -1,31 +1,41 @@
 import styled from 'styled-components'
 
 export const SharedImage = styled.div`
+  position: relative;
+
   width: 100.1rem;
   height: 50.6rem;
   padding-top: 3rem;
-  padding-left: 3rem;
-  padding-right: 3rem;
   margin: 0 auto;
-
-  background-color: #2d152b;
 
   display: grid;
   grid-template-rows: auto 1fr auto;
+
+  background-color: rgba(31, 41, 55, 1);
+
+  /* background-image: url('/assets/bg-products.png');
+  background-repeat: no-repeat;
+  background-size: 100rem;
+  background-position-x: 50%; */
+  /* box-shadow: rgb(21, 17, 23) 0px -20px 20px 0px inset; */
 `
 
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  padding: 0 3rem;
 `
 
 export const Title = styled.div`
   display: flex;
   align-items: center;
-  gap: 2.2rem;
 
   h1 {
+    margin-left: 1.8rem;
+    margin-right: 1.6rem;
+
     font-size: 4rem;
     font-weight: 300;
   }
@@ -33,6 +43,7 @@ export const Title = styled.div`
 
 export const Detail = styled.div`
   padding: 0.8rem 1rem;
+  margin-right: 2rem;
 
   background: rgba(0, 0, 0, 0.19);
   border-radius: 1rem;
@@ -82,6 +93,7 @@ export const Main = styled.main`
   align-items: center;
 
   margin-top: 1rem;
+  padding: 0 3rem;
 `
 
 export const InfoContainer = styled.div`
@@ -151,9 +163,11 @@ export const Footer = styled.footer`
   padding-top: 1.6rem;
   padding-bottom: 1.6rem;
   margin-top: 2rem;
+  padding: 1.6rem 3rem;
 
   background-color: #2d152b;
 `
+
 export const SocialMedia = styled.div`
   display: flex;
   gap: 0.8rem;
@@ -162,154 +176,13 @@ export const SocialMedia = styled.div`
   font-weight: 300;
 `
 
-// Small
+export const BackgroundContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  top: 0;
 
-// import styled from 'styled-components'
-
-// export const SharedImage = styled.div`
-//   width: 50.7rem;
-//   height: 25.3rem;
-//   padding-top: 2rem;
-//   padding-left: 2rem;
-//   padding-right: 2rem;
-
-//   background-color: #2d152b;
-
-//   display: grid;
-//   grid-template-rows: auto 1fr auto;
-// `
-
-// export const Header = styled.header`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-// `
-
-// export const Title = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 0.8rem;
-
-//   h1 {
-//     font-size: 2rem;
-//     font-weight: 300;
-//   }
-// `
-
-// export const Detail = styled.div`
-//   padding: 0.4rem 0.6rem;
-
-//   background: rgba(0, 0, 0, 0.19);
-//   border-radius: 1rem;
-
-//   font-size: 0.9rem;
-//   font-weight: 300;
-// `
-
-// export const HorizontalLine = styled.div`
-//   width: 6rem;
-//   height: 2px;
-//   background-color: rgba(255, 255, 255, 0.3);
-// `
-
-// export const UserInfo = styled.div`
-//   display: flex;
-//   gap: 1.8rem;
-
-//   h2 {
-//     font-size: 1.4rem;
-//     font-weight: 700;
-//   }
-// `
-
-// export const Profile = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 1.8rem;
-// `
-
-// export const ProfileImage = styled.div`
-//   width: 4rem;
-//   height: 4rem;
-
-//   background-color: red;
-//   border-radius: 50%;
-// `
-
-// export const ProfileAddress = styled.p`
-//   font-size: 1.2rem;
-//   font-weight: 300;
-// `
-
-// export const Main = styled.main`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-
-//   margin-top: 1rem;
-// `
-
-// export const InfoContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 1.6rem;
-// `
-
-// export const Info = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 1.1rem;
-// `
-// export const InfoTitle = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 1.3rem;
-
-//   span {
-//     font-size: 1.8rem;
-//     font-weight: 300;
-//   }
-// `
-
-// export const InfoValue = styled.div`
-//   font-weight: 500;
-//   color: #5ee56b;
-//   font-size: 5.6rem;
-// `
-
-// export const Assets = styled.div`
-//   font-size: 1.8rem;
-//   font-weight: 300;
-// `
-
-// export const AssetsContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 2.4rem;
-
-//   margin-top: 1.6rem;
-// `
-
-// export const ChartContainer = styled.div`
-//   width: 100%;
-//   max-width: 50.7rem;
-//   /* height: 100%; */
-// `
-
-// export const Footer = styled.footer`
-//   display: flex;
-//   gap: 1.6rem;
-
-//   padding-top: 1.6rem;
-//   padding-bottom: 1.6rem;
-//   margin-top: 2rem;
-
-//   background-color: #2d152b;
-// `
-// export const SocialMedia = styled.div`
-//   display: flex;
-//   gap: 0.8rem;
-
-//   font-size: 1.8rem;
-//   font-weight: 300;
-// `
+  svg {
+    width: 100.1rem;
+    height: 100%;
+  }
+`
