f9d547a75ab65a81cfaa1998e1dcace677b1c250
Tiago138 <tgnvsctn@gmail.com>
2022-05-13 14:11:07 -0300

    feat: configured the data to pass to the staking table component

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,48 +1,137 @@
-import React from 'react'
+import useStakingContract from '../../hooks/useStakingContract'
+import { Staking } from '../../constants/tokenAddresses'
 
 import PortfolioHeading from '../../components/PortfolioHeading'
 import AssetsTable from './AssetsTable'
+import StakingTable from './StakingTable'
+import AnyCard from '../AnyCard'
 
 import AssetsIcon from '../../../public/assets/iconGradient/section-title-Assets.svg'
 import StakedPoolsIcon from '../../../public/assets/iconGradient/section-title-Staked-pools.svg'
 import aHYPE from '../../../public/assets/ahype.svg'
 import AvalancheIcon from '../../../public/assets/avalancheIcon.svg'
+import iconKace from '../../../public/assets/logo-kacy-stake.svg'
+import iconPangolin from '../../../public/assets/logo-pangolin-40x40.svg'
+import iconTraderJoe from '../../../public/assets/logo-traderJoe.svg'
 
+import { BNtoDecimal } from '../../utils/numerals'
 import * as S from './styles'
-import StakingTable from './StakingTable'
 
+interface IstakedTokenType {
+  [key: number]: {
+    symbol: string,
+    poolName: string,
+    icons: string[],
+    network: string,
+    networkIcon: string
+  };
+}
+
+interface IStakesType {
+  pid: number;
+  symbol: string;
+  poolName: string;
+  icons: string[];
+  network: string;
+  networkIcon: string;
+  amount: string;
+  pendingRewards: string;
+  rewardPerTokenPaid: string;
+}
 const Portfolio = () => {
-  const assetsArr = [
-    {
-      name: 'Avalanche Hype Index',
+  const { userInfo } = useStakingContract(Staking)
+  const [stakes, setStakes] = useState<IStakesType[]>([])
+  const stakedToken: IstakedTokenType = {
+    0: {
+      symbol: 'KACY',
+      poolName: 'KACY',
+      icons: [iconKace],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    1: {
+      symbol: 'KACY',
+      poolName: 'KACY',
+      icons: [iconKace],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    2: {
+      symbol: 'KACY',
+      poolName: 'KACY',
+      icons: [iconKace],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    3: {
+      symbol: 'KACY',
+      poolName: 'KACY',
+      icons: [iconKace],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    4: {
+      symbol: 'KACY',
+      poolName: 'KACY',
+      icons: [iconKace],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    5: {
+      symbol: 'LP',
+      poolName: 'KACY-AVAX PNG LP',
+      icons: [iconKace, AvalancheIcon, iconPangolin],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
+    },
+    6: {
       symbol: 'aHYPE',
+      poolName: 'aHYPE',
+      icons: [aHYPE],
       network: 'Avalanche',
-      networkIcon: AvalancheIcon,
-      fundIcon: aHYPE,
-      price: '394,34',
-      tvl: '785,345.67',
-      monthUp: 28,
-      dayUp: -2,
-      balance: '4,000,000.00',
-      balanceUSD: '$10,000,000.00'
+      networkIcon: AvalancheIcon
+    },
+    7: {
+      symbol: 'LP-JOE',
+      poolName: 'KACY-AVAX JOE LP',
+      icons: [iconKace, AvalancheIcon, iconTraderJoe],
+      network: 'Avalanche',
+      networkIcon: AvalancheIcon
     }
-  ]
+  }
+  React.useEffect(() => {
+    if (!userWalletAddress) {
+      return
+    }
 
-  const stakingArr = [
-    {
-      name: '$KACY-AVAX PNG LP',
-      poolIcons: [aHYPE, aHYPE, aHYPE],
-      network: 'Avalanche',
-      networkIcon: AvalancheIcon,
-      apy: '394,34',
-      tvl: '785,345.67',
-      staked: '4,000,000.00',
-      stakedUSD: '$10,000,000.00',
-      reward: '4,000,000.00',
-      rewardUSD: '$10,000,000.00',
-      token: 'kacy'
+    const pids = [0, 1, 2, 3, 4, 5, 6, 7]
+
+    const arr: IStakesType[] = []
+    const asyncFunc = async () => {
+      for (let pid = 0; pid < pids.length - 1; pid++) {
+        const userInfoResponse = await userInfo(pid, userWalletAddress)
+
+        if ((await userInfoResponse.amount) > 0) {
+          arr.push({
+            pid: pid,
+            symbol: stakedToken[pid].symbol,
+            poolName: stakedToken[pid].poolName,
+            icons: stakedToken[pid].icons,
+            network: stakedToken[pid].network,
+            networkIcon: stakedToken[pid].networkIcon,
+            amount: userInfoResponse.amount,
+            pendingRewards: userInfoResponse.pendingRewards,
+            rewardPerTokenPaid: userInfoResponse.rewardPerTokenPaid
+          })
+        }
     }
-  ]
+
+      const results = await Promise.all(arr)
+
+      setStakes(results)
+    }
+    asyncFunc()
+  }, [userWalletAddress, userInfo])
 
   return (
     <>
@@ -68,9 +157,36 @@ const Portfolio = () => {
         />
       </S.paddingWrapper>
 
+      {stakes[0] && parseFloat(stakes[0].amount) > 0 ? (
       <S.paddingLeftWrapper>
-        <StakingTable stakes={stakingArr} />
+          <StakingTable
+            stakes={stakes}
+            walletAddress={userWalletAddress}
+            setTotalStaked={setTotalStaked}
+          />
       </S.paddingLeftWrapper>
+      ) : (
+        <S.paddingWrapper>
+          {userWalletAddress ? (
+            <AnyCard
+              text="It seems you have not staked any KACY"
+              button={true}
+              link="/farm"
+              buttonText="I Want To Stake Some Tokens"
+            />
+          ) : (
+            <AnyCard
+              text=""
+              button2={true}
+              buttonText="Connect Wallet"
+              onClick={() => {
+                setIsModalWallet(true)
+              }}
+            />
+          )}
+        </S.paddingWrapper>
+      )}
+
     </>
   )
 }
