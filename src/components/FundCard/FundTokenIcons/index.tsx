2438d2c4f932f172474c04f0c10f4551d5e68ec8
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-05-25 13:05:46 -0300

    refactor: the way fundcard gets images from coingecko

--- src/components/FundCard/FundTokenIcons/index.tsx
+++ src/components/FundCard/FundTokenIcons/index.tsx
@@ -6,21 +6,30 @@ import none from '../../../../public/assets/coming-soon.svg'
 import * as S from './styles'
 
 interface TokenIconsProps {
-  tokens: string[][];
+  tokens: { [key: string]: string };
   poolInfo: any[];
 }
 
 const FundTokenIcons = ({ tokens, poolInfo }: TokenIconsProps) => {
   return (
     <S.Container>
-      {tokens.map((entry: string[], index: number) => {
-        return (
-          <S.ImageWrapper key={index}>
-            <Image src={entry[1] || none} alt="" width={16} height={16} />
-            <span>{(poolInfo[index].weight_normalized * 100).toFixed(2)}%</span>
-          </S.ImageWrapper>
-        )
-      })}
+      {poolInfo
+        .slice(0, poolInfo.length >= 3 ? 3 : poolInfo.length)
+        .map((asset, index) => {
+          return (
+            <S.ImageWrapper key={index}>
+              <Image
+                src={tokens[asset.token.id] || none.src}
+                alt=""
+                width={16}
+                height={16}
+              />
+              <span>
+                {(poolInfo[index].weight_normalized * 100).toFixed(2)}%
+              </span>
+            </S.ImageWrapper>
+          )
+        })}
     </S.Container>
   )
 }
