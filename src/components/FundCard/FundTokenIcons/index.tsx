dbb7370ddd4cc32c2746879ec98a9fedc15de209
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-06-03 12:16:11 -0300

    fix: mapped wraped tokens for receive images

--- src/components/FundCard/FundTokenIcons/index.tsx
+++ src/components/FundCard/FundTokenIcons/index.tsx
@@ -10,6 +10,15 @@ interface TokenIconsProps {
   poolInfo: any[];
 }
 
+const addressChanger: { [key: string]: string } = {
+  '0xe28Ad9Fa07fDA82abab2E0C86c64A19D452b160E':
+    '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab', // WETH
+  '0x964555644E067c560A4C144360507E80c1104784':
+    '0xc7198437980c041c805a1edcba50c1ce5db95118', //USDT
+  '0xbbcED92AC9B958F88A501725f080c0360007e858':
+    '0x50b7545627a5162f82a992c33b87adc75187b218' //WBTC
+}
+
 const FundTokenIcons = ({ tokens, poolInfo }: TokenIconsProps) => {
   return (
     <S.Container>
@@ -19,7 +28,10 @@ const FundTokenIcons = ({ tokens, poolInfo }: TokenIconsProps) => {
           return (
             <S.ImageWrapper key={index}>
               <Image
-                src={tokens[asset.token.id] || none.src}
+                src={
+                  tokens[addressChanger[asset.token.id] ?? asset.token.id] ||
+                  none.src
+                }
                 alt=""
                 width={16}
                 height={16}
