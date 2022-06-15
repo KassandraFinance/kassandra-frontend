70155f87bd0a4e26a639bfc9fc6e05466a4afa2d
ChristianDaniel <christian_daniel04@hotmail.com>
2022-06-15 14:49:22 -0300

    feat: add SelectTabs component in Profile page

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -9,16 +9,40 @@ import Breadcrumb from '../../components/Breadcrumb'
 import BreadcrumbItem from '../../components/Breadcrumb/BreadcrumbItem'
 import Portfolio from '../../components/Portfolio'
 import Web3Disabled from '../../components/Web3Disabled'
+import UserDescription from '../../components/Governance/UserDescription'
+import SelectTabs from '../../components/SelectTabs'
+import AnyCard from '../../components/AnyCard'
+import Loading from '../../components/Loading'
+
+import profileIcon from '../../../public/assets/iconGradient/profile.svg'
+import walletIcon from '../../../public/assets/iconGradient/wallet-gradient.svg'
+import governanceIcon from '../../../public/assets/iconGradient/vote.svg'
 
 import substr from '../../utils/substr'
 
 import * as S from './styles'
 
+const tabs = [
+  {
+    text: 'Portfolio',
+    icon: walletIcon
+  },
+  {
+    text: 'Managed Funds',
+    icon: profileIcon
+  },
+  {
+    text: 'Governance Data',
+    icon: governanceIcon
+  }
+]
+
 const Profile = () => {
-  const { userWalletAddress } = useSelector((state: RootStateOrAny) => state)
   const router = useRouter()
+  const { userWalletAddress } = useSelector((state: RootStateOrAny) => state)
 
   const [hasEthereumProvider, setHasEthereumProvider] = React.useState(false)
+  const [isSelectTab, setIsSelectTab] = React.useState<string>('portfolio')
 
   const profileAddress = router.query.profileAddress
 
@@ -35,6 +59,24 @@ const Profile = () => {
 
     checkEthereumProvider()
   })
+
+  React.useEffect(() => {
+    const asPath = router.asPath.slice(52)
+
+    switch (asPath) {
+      case 'portfolio':
+        return setIsSelectTab('Portfolio')
+
+      case 'managed-funds':
+        return setIsSelectTab('Managed Funds')
+
+      case 'governance-data':
+        return setIsSelectTab('Governance Data')
+
+      default:
+        return setIsSelectTab('Portfolio')
+    }
+  }, [router])
 
   return (
     <>
@@ -54,23 +96,41 @@ const Profile = () => {
         )}
       </Breadcrumb>
 
-      {hasEthereumProvider ? (
-        <Portfolio
-          profileAddress={
-            typeof profileAddress === 'undefined'
-              ? ''
-              : typeof profileAddress === 'string'
-              ? profileAddress
-              : ''
-          }
+      <S.ProfileContainer>
+        <UserDescription />
+        <SelectTabs
+          tabs={tabs}
+          isSelect={isSelectTab}
+          setIsSelect={setIsSelectTab}
         />
-      ) : (
-        <Web3Disabled
-          textHeader="You need to have a Wallet installed"
-          bodyText="Please install any Wallet to see the users profiles"
-          type="connect"
-        />
-      )}
+        {isSelectTab === 'Portfolio' ? (
+          <>
+            {hasEthereumProvider ? (
+              <Portfolio
+                profileAddress={
+                  typeof profileAddress === 'undefined'
+                    ? ''
+                    : typeof profileAddress === 'string'
+                    ? profileAddress
+                    : ''
+                }
+              />
+            ) : (
+              <Web3Disabled
+                textHeader="You need to have a Wallet installed"
+                bodyText="Please install any Wallet to see the users profiles"
+                type="connect"
+              />
+            )}
+          </>
+        ) : isSelectTab === 'Managed Funds' ? (
+          <AnyCard text="Coming Soon…" />
+        ) : isSelectTab === 'Governance Data' ? (
+          <h1>Governance Data</h1>
+        ) : (
+          <Loading marginTop={4} />
+        )}
+      </S.ProfileContainer>
     </>
   )
 }
--- src/templates/Profile/styles.ts
+++ src/templates/Profile/styles.ts
@@ -1,2 +1,14 @@
 import styled from 'styled-components'
-import theme from '../../styles/theme'
+
+export const ProfileContainer = styled.section`
+  max-width: 114rem;
+  margin: 0 auto;
+
+  @media (max-width: 1200px) {
+    padding: 0 3rem;
+  }
+
+  @media (max-width: 540px) {
+    padding: 0 1.6rem;
+  }
+`
