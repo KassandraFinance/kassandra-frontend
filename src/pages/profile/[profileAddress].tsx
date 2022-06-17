56d137f695921f3c9ab866704377bbcb60c3faf7
ChristianDaniel <christian_daniel04@hotmail.com>
2022-06-17 13:42:01 -0300

    refactor: add SWRConfig in Profile page

--- src/pages/profile/[profileAddress].tsx
+++ src/pages/profile/[profileAddress].tsx
@@ -1,6 +1,16 @@
+import { SWRConfig } from 'swr'
 import Profile from '../../templates/Profile'
 import NotFound from '../../templates/404'
 
 export default function Index() {
-  return process.env.NEXT_PUBLIC_VOTE === '1' ? <Profile /> : <NotFound />
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 30000,
+        fetcher: url => fetch(url).then(res => res.json())
+      }}
+    >
+      {process.env.NEXT_PUBLIC_VOTE === '1' ? <Profile /> : <NotFound />}
+    </SWRConfig>
+  )
 }
