b1f8b25cb8b9c9ae3235759fcc2b2e9380ccd410
ChristianDaniel <christian_daniel04@hotmail.com>
2022-07-06 22:23:06 -0300

    refactor: change About icon mobile in ModalSocialMediaMobile

--- src/components/Modals/ModalSocialMediaMobile/index.tsx
+++ src/components/Modals/ModalSocialMediaMobile/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import discord from '../../../../public/assets/socialMidia/discord.svg'
 import telegram from '../../../../public/assets/socialMidia/telegram.svg'
 import twitter from '../../../../public/assets/socialMidia/twitter.svg'
 import medium from '../../../../public/assets/socialMidia/medium.svg'
 import github from '../../../../public/assets/socialMidia/github.svg'
-import kacy64 from '../../../../public/assets/logos/kacy-64.svg'
+import kacyWhite from '../../../../public/assets/logos/kacy-white.svg'
 
 import * as S from './styles'
-import { useRouter } from 'next/router'
 
 interface IModalSocialMediaMobileProps {
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -75,7 +75,7 @@ const ModalSocialMediaMobile = ({
             setModalOpen(false), router.push('/about')
           }}
         >
-          <Image src={kacy64} alt="Github" width={24} height={24} />
+          <Image src={kacyWhite} alt="" width={24} height={24} />
           <span>About</span>
         </S.SocialIcon>
       </S.ModalContainer>
