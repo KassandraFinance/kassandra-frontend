53a711523afbbc5c9f931b660d91d210a4b8f1b2
ChristianDaniel <christian_daniel04@hotmail.com>
2022-07-13 16:22:19 -0300

    refactor: add loading in explore page

--- src/templates/Explore/index.tsx
+++ src/templates/Explore/index.tsx
@@ -1,19 +1,30 @@
+import React from 'react'
+
 import Header from '../../components/Header'
 import Breadcrumb from '../../components/Breadcrumb'
 import BreadcrumbItem from '../../components/Breadcrumb/BreadcrumbItem'
 import TitleSection from '../../components/TitleSection'
 import FundCard from '../../components/FundCard'
+import AnyCard from '../../components/AnyCard'
+import Loading from '../../components/Loading'
 
 import sectionTitleEye from '../../../public/assets/iconGradient/section-title-eye.svg'
 import featuredFunds from '../../../public/assets/iconGradient/featured.svg'
 import communityFunds from '../../../public/assets/iconGradient/community.svg'
 
+import { products } from '../../constants/tokenAddresses'
+
 import * as S from './styles'
 
-import { products } from '../../constants/tokenAddresses'
-import AnyCard from '../../components/AnyCard'
-
 export default function Explore() {
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2200)
+  }, [])
+
   return (
     <>
       <Header />
@@ -37,7 +48,13 @@ export default function Explore() {
         <S.ExploreContainer>
           <TitleSection image={featuredFunds} title="Featured Funds" text="" />
 
-          <S.CardContainer>
+          {loading && (
+            <S.LoadingContainer>
+              <Loading marginTop={0} />
+            </S.LoadingContainer>
+          )}
+
+          <S.CardContainer loading={loading}>
             {products.map((product, index: number) => {
               return <FundCard key={index} product={product} />
             })}
