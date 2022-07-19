acb2394f8f1bdee38ad4d686202bada2fd4332f1
ChristianDaniel <christian_daniel04@hotmail.com>
2022-07-19 09:43:09 -0300

    refactor: add catch on fetch in userDescription component

--- src/components/Governance/UserDescription/index.tsx
+++ src/components/Governance/UserDescription/index.tsx
@@ -58,10 +58,15 @@ const UserDescription = ({ userWalletUrl }: IUserDescriptionProps) => {
     : userWalletUrl
 
   React.useEffect(() => {
-    if (!userWalletUrl) return
+    if (!walletUserString) return
 
-    fetch(`/api/profile/${userWalletUrl}`)
-      .then(res => res.json())
+    fetch(`/api/profile/${walletUserString}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw new Error('Something went wrong')
+      })
       .then(data => {
         const { image, ...profile } = data
 
@@ -71,7 +76,18 @@ const UserDescription = ({ userWalletUrl }: IUserDescriptionProps) => {
         }),
           setImageUser({ url: image, isNFT: data.isNFT })
       })
-  }, [userWalletAddress, userWalletUrl, isOpenModal])
+      .catch(error => {
+        setUserData({
+          nickname: '',
+          twitter: '',
+          website: '',
+          telegram: '',
+          discord: '',
+          description: ''
+        }),
+          setImageUser({ url: '', isNFT: false })
+      })
+  }, [isOpenModal, userWalletUrl])
 
   React.useEffect(() => {
     if (window.screen.width > 768) {
@@ -320,3 +336,17 @@ const UserDescription = ({ userWalletUrl }: IUserDescriptionProps) => {
 }
 
 export default UserDescription
+
+// fetch(`/api/profile/${walletUserString}`)
+//   .then(res => res.json())
+//   .catch(error => console.log(error))
+//   .then(data => {
+//     const { image, ...profile } = data
+
+//     setUserData({
+//       ...profile,
+//       description: profile.description ?? ''
+//     }),
+//       setImageUser({ url: image, isNFT: data.isNFT })
+//   })
+//   .catch(error => console.log(error))
