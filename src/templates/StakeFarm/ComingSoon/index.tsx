69ce7341510d8abf5df3c1faa05f903e4d557759
Tiago138 <tgnvsctn@gmail.com>
2022-07-26 17:24:41 -0300

    refactor: moved stake and farm to each component

---
+++ src/templates/StakeFarm/ComingSoon/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const ComingSoon = () => {
+  return (
+    <S.BorderGradient>
+      <S.ComingSoon>
+        <img src="/assets/icons/coming-soon.svg" alt="" />
+        <h2>Coming Soon</h2>
+      </S.ComingSoon>
+    </S.BorderGradient>
+  )
+}
+
+export default ComingSoon
