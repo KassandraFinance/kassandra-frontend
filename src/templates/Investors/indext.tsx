a2f35966c8eb73f0ab0f65a9ea3431621fed3b1b
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-07-28 16:02:56 -0300

    feat: add section protocol description

--- src/templates/Investors/indext.tsx
+++ src/templates/Investors/indext.tsx
@@ -2,7 +2,14 @@ import Button from '../../components/Button'
 import Header from '../../components/Header'
 import { products } from '../../constants/tokenAddresses'
 import FundCard from './FundCard'
+
+import certik from '../../../public/assets/iconGradient/certik.svg'
+import lock from '../../../public/assets/iconGradient/lock.svg'
+import walletLock from '../../../public/assets/iconGradient/wallet-lock.svg'
+
 import * as S from './styles'
+import Image from 'next/image'
+import ExternalLink from '../../components/ExternalLink'
 
 const Investors = () => {
   return (
@@ -13,7 +20,7 @@ const Investors = () => {
           <S.Title>
             <span>01</span>
             <hr />
-            <span>Investors</span>
+            <span>INVESTORS</span>
           </S.Title>
           <h1>
             Diversify with simple steps through trustworthy managers and
@@ -44,6 +51,59 @@ const Investors = () => {
               <FundCard key={product.symbol} fund={product} />
             ))}
           </S.FundsCards>
+          <S.Protocol>
+            <S.DescriptionProtocol>
+              <span>PROTOCOL SAFETY</span>
+              <h1>Make the most of your money while keeping it safe</h1>
+              <span>
+                This is how we ensure that user funds are always protected. All
+                infrastructure is governed by up-to-date smart contracts and
+                audited by trusted players in the space.
+              </span>
+              <ExternalLink
+                hrefLink="https://kassandrafoundation.medium.com/how-kassandra-works-ac50630601f6"
+                text="Learn more"
+              />
+            </S.DescriptionProtocol>
+            <S.DetailsProtocol>
+              <S.Detail>
+                <S.Icon>
+                  <Image src={walletLock} />
+                </S.Icon>
+                <S.Topic>
+                  <span>NON CUSTODIAL</span>
+                  <p>
+                    Your assets never leave your wallet. You’re always in
+                    control of your funds and you can pull them out anytime.
+                  </p>
+                </S.Topic>
+              </S.Detail>
+              <S.Detail>
+                <S.Icon>
+                  <Image src={lock} />
+                </S.Icon>
+                <S.Topic>
+                  <span>PERMISSIONLESS</span>
+                  <p>
+                    Invest when you want and how you want it. Transactions are
+                    fast, automatic and in your complete control.
+                  </p>
+                </S.Topic>
+              </S.Detail>
+              <S.Detail>
+                <S.Icon>
+                  <Image src={certik} />
+                </S.Icon>
+                <S.Topic>
+                  <span>CERTIK AUDIT</span>
+                  <p>
+                    Our protocol was thoroughly audited by Certik, and you can
+                    verify this information here.
+                  </p>
+                </S.Topic>
+              </S.Detail>
+            </S.DetailsProtocol>
+          </S.Protocol>
         </S.Products>
       </S.InvestorsContainer>
     </>
