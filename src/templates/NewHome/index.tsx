f449894be9317c24504bd5964d8f40bd00cf3575
Tiago138 <tgnvsctn@gmail.com>
2022-07-28 15:45:27 -0300

    feat: created new home page template

---
+++ src/templates/NewHome/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HeroHome from './HeroHome'
+
+import * as S from './styles'
+
+const NewHome = () => {
+  return (
+    <S.Container>
+      <HeroHome />
+    </S.Container>
+  )
+}
+
+export default NewHome
