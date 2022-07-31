b7ded309d413f31e8b9ce9efeb8d1039a8d84a6d
Jony <jonyreiscardoso@gmail.com>
2022-07-31 17:02:00 -0400

    refactor: move OutputTokenValue component to PoolOperations

--- src/components/PoolOperations/Form/InputTokens.tsx
+++ src/components/PoolOperations/Form/InputTokens.tsx
@@ -6,8 +6,8 @@ import { ITokenDetails } from '../../../context/PoolTokensContext'
 
 import useMatomoEcommerce from '../../../hooks/useMatomoEcommerce'
 
-import InputTokenValue from '../../InputTokenValue'
-import OutputTokenValue from '../../OutputTokenValue/OutputTokenValue'
+import InputTokenValue from '../InputTokenValue'
+import OutputTokenValue from '../OutputTokenValue'
 import SelectInput from '../../SelectInput'
 
 import { BNtoDecimal } from '../../../utils/numerals'
