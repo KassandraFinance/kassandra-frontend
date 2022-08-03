5a45948a55ca7016be3a7a05601e07c57dbf3b75
Edmar Alencar <edmaaralencar1@gmail.com>
2022-08-03 11:45:23 -0300

    feat: create Scarcity component

---
+++ src/templates/TokenHolder/Scarcity/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  margin: 0 1.6rem;
+
+  h2 {
+    margin-bottom: 2.4rem;
+    text-align: center;
+
+    @media (max-width: 550px) {
+      font-size: 2.4rem;
+      text-align: left;
+    }
+  }
+
+  p {
+    @media (max-width: 550px) {
+      font-size: 1.4rem;
+      text-align: left;
+    }
+  }
+`
+
+export const ImageWrapper = styled.div`
+  position: relative;
+  margin: 0 auto;
+
+  width: 100%;
+  max-width: 120rem;
+
+  height: 70rem;
+
+  @media (max-width: 800px) {
+    margin-top: 4rem;
+    max-width: 83rem;
+    height: 45rem;
+  }
+
+  @media (max-width: 550px) {
+    margin-top: 1.8rem;
+    max-width: 42.5rem;
+    height: 30rem;
+  }
+`
