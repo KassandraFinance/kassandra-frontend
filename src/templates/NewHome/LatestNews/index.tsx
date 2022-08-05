0d345dfb8003b28b0d5faf6eac764d97e89e6b44
Tiago138 <tgnvsctn@gmail.com>
2022-08-05 11:59:01 -0300

    feat: configured to get data from medium feed

--- src/templates/NewHome/LatestNews/index.tsx
+++ src/templates/NewHome/LatestNews/index.tsx
@@ -8,25 +8,62 @@ import NewsCard from './NewsCard'
 
 import * as S from './styles'
 
+interface IMediumPost {
+  author: string;
+  categories: string[];
+  content: string;
+  description: string;
+  enclosure: {};
+  guid: string;
+  link: string;
+  pubDate: string;
+  thumbnail: string;
+  title: string;
+}
+
+const API_URL =
+  'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@kassandrafoundation'
+
 const LatestNews = () => {
   const [mediumPosts, setMediumPosts] = React.useState<IMediumPost[]>([])
+
+  const fetcher = async (url: string) => {
+    const res = await fetch(url)
+    return res.json()
+  }
+
+  const { data } = useSWR(API_URL, fetcher)
 
   const responsive = {
     0: { items: 1 },
     730: { items: 2 },
     1250: { items: 3 }
   }
+
   const items = mediumPosts.map(post => {
     return (
       <NewsCard
+        key={post.title}
+        thumbnail={post.thumbnail}
+        title={post.title}
+        description={post.content}
+        link={post.link}
       />
     )
   })
+
+  React.useEffect(() => {
+    if (data) {
+      setMediumPosts(data.items)
+    }
+  }, [data])
+
   return (
     <S.LatestNewsContainer>
       <LatestNewsHeader />
 
       <S.NewsCardContainer>
+        {data && (
           <AliceCarousel
             mouseTracking
             infinite
@@ -34,6 +71,7 @@ const LatestNews = () => {
             items={items}
             responsive={responsive}
           />
+        )}
       </S.NewsCardContainer>
     </S.LatestNewsContainer>
   )
