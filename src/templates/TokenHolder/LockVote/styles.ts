192e445a2b48153854cc7277637328ccb0ec9491
Edmar Alencar <edmaaralencar1@gmail.com>
2022-08-04 22:03:18 -0300

    style: fix LockVote width

--- src/templates/TokenHolder/LockVote/styles.ts
+++ src/templates/TokenHolder/LockVote/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Button } from '../styles'
 
 export const Wrapper = styled.section`
-  max-width: 118rem;
+  max-width: 96rem;
   width: 100%;
 
   margin: 0 auto;
