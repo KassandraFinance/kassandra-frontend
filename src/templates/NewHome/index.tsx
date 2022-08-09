b458e8f2a617988e60eae9b38804341d18088418
Tiago138 <tgnvsctn@gmail.com>
2022-08-09 10:39:57 -0300

    feat: add contribute component

--- src/templates/NewHome/index.tsx
+++ src/templates/NewHome/index.tsx
@@ -9,6 +9,7 @@ import ManagerSection from './ManagerSection'
 import DaoSection from './DaoSection'
 import KacySection from './KacySection'
 import LatestNews from './LatestNews'
+import Contribute from '../TokenHolder/Contribute'
 
 import light3 from '../../../public/assets/images/backgroundHome/light-mobile3.png'
 import lightTable5 from '../../../public/assets/images/backgroundHome/light-tablet5.png'
@@ -65,6 +66,8 @@ const NewHome = () => {
       </S.Container>
 
       <LatestNews />
+
+      <Contribute />
     </S.HomeBackgroundContainer>
   )
 }
