324c24c108b7ecacc70e2d1bb5ccacb0067d6afc
ChristianDaniel <christian_daniel04@hotmail.com>
2022-08-12 19:32:36 -0300

    feat: add OptionButton in header mobile

--- src/components/Header/newHeader.tsx
+++ src/components/Header/newHeader.tsx
@@ -6,9 +6,11 @@ import { useRouter } from 'next/router'
 import useMatomoEcommerce from '../../hooks/useMatomoEcommerce'
 
 import Button from '../Button'
+import ModalSocialMediaMobile from '../Modals/ModalSocialMediaMobile'
 
 import kacy96 from '../../../public/assets/logos/kacy-96.svg'
 import logoKassandra from '../../../public/assets/logos/kassandra-header.svg'
+import options from '../../../public/assets/utilities/options.svg'
 
 import * as S from './newStylesHeader'
 
@@ -18,6 +20,7 @@ export type MenuProps = {
 
 const Header = () => {
   const { trackEventFunction } = useMatomoEcommerce()
+  const [isOpenModal, setIsOpenModal] = React.useState(false)
 
   const router = useRouter()
   const pathName = router.pathname
@@ -67,6 +70,14 @@ const Header = () => {
                 text="Launch App"
               />
             </S.ButtonsWrapper>
+            <S.OptionsContainer>
+              <S.ButtonOptions onClick={() => setIsOpenModal(true)}>
+                <Image src={options} alt="options" />
+              </S.ButtonOptions>
+            </S.OptionsContainer>
+            {isOpenModal && (
+              <ModalSocialMediaMobile setModalOpen={setIsOpenModal} />
+            )}
           </S.MenuBottom>
         </S.Menu>
       </S.Wrapper>
