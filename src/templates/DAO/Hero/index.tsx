901d4899f05beef7885a56dfaf5af987146c1789
Tiago138 <tgnvsctn@gmail.com>
2022-08-12 17:27:45 -0300

    feat: buy kacy btn open modal

--- src/templates/DAO/Hero/index.tsx
+++ src/templates/DAO/Hero/index.tsx
@@ -1,44 +1,61 @@
+import React from 'react'
 import Image from 'next/image'
 
 import Button from '../../../components/Button'
 import Scroll from '../../../components/Scroll'
+import ModalBuyKacy from '../../../components/Modals/ModalBuyKacy'
 
 import * as S from './styles'
 
-const Hero = () => (
-  <S.HeroContainer>
-    <S.ImageContent />
+const Hero = () => {
+  const [isOpenModal, setIsOpenModal] = React.useState<boolean>(false)
 
-    <S.HeroContent>
-      <S.ManagerNumberPage>
-        <p>03</p>
-        <span />
-        <p>DAO</p>
-      </S.ManagerNumberPage>
-      <S.HeroDescription>
-        <h1>It’s our community, make your voice heard</h1>
-        <p>
-          Shape Kassandra into what you believe in. Propose, vote and contribute
-          where its needed. Help the community flourish: the bigger the pie, the
-          better your slice.
-        </p>
+  return (
+    <>
+      <S.HeroContainer>
+        <S.ImageContent />
 
-        <Button
-          className="button"
-          size="huge"
-          icon={
-            <Image src="/assets/iconGradient/kacy.svg" width={18} height={18} />
-          }
-          backgroundPrimary
-          text="Buy KACY"
-        />
-      </S.HeroDescription>
-    </S.HeroContent>
+        <S.HeroContent>
+          <S.ManagerNumberPage>
+            <p>03</p>
+            <span />
+            <p>DAO</p>
+          </S.ManagerNumberPage>
+          <S.HeroDescription>
+            <h1>It’s our community, make your voice heard</h1>
+            <p>
+              Shape Kassandra into what you believe in. Propose, vote and
+              contribute where its needed. Help the community flourish: the
+              bigger the pie, the better your slice.
+            </p>
 
-    <S.ScrollContainer>
-      <Scroll />
-    </S.ScrollContainer>
-  </S.HeroContainer>
-)
+            <Button
+              className="button"
+              size="huge"
+              icon={
+                <Image
+                  src="/assets/iconGradient/kacy.svg"
+                  width={18}
+                  height={18}
+                />
+              }
+              backgroundPrimary
+              text="Buy KACY"
+              onClick={() => {
+                setIsOpenModal(true)
+              }}
+            />
+          </S.HeroDescription>
+        </S.HeroContent>
+
+        <S.ScrollContainer>
+          <Scroll />
+        </S.ScrollContainer>
+      </S.HeroContainer>
+
+      <ModalBuyKacy modalOpen={isOpenModal} setModalOpen={setIsOpenModal} />
+    </>
+  )
+}
 
 export default Hero
