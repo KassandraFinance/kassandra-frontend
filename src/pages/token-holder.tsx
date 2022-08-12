e198df900c350a9ad6820731c0e3339b688deb34
Tiago138 <tgnvsctn@gmail.com>
2022-08-12 13:37:06 -0300

    feat: add custom title to instituctional pages

--- src/pages/token-holder.tsx
+++ src/pages/token-holder.tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head'
+
 import TokenHolder from '../templates/TokenHolder'
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://kassandra.finance/kacy-og.png"
+        />
+        <meta property="og:image:width" content="1012" />
+        <meta property="og:image:height" content="506" />
+        <meta property="og:url" content="https://kassandra.finance/" />
+        <title>Kassandra DAO</title>
+      </Head>
       <TokenHolder />
     </>
   )
