3a21436afaecfb5bcefb5ec9a11631fcee314b55
Tiago138 <tgnvsctn@gmail.com>
2022-08-16 15:49:19 -0300

    refactor: removed # from k3c

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -186,7 +186,7 @@ const tricrypto: ProductDetails  = process.env.NEXT_PUBLIC_MASTER === '1' ?
     fundIcon: tricryptoIcon,
     pid: 8,
     fundLink: 'https://kassandrafoundation.medium.com/kassandras-new-autocompounding-strategy-tricrypto-185eb1fca2c2',
-    fundSummary: `The #K3C represents a yield-earning portfolio composed of BTC,
+    fundSummary: `The K3C represents a yield-earning portfolio composed of BTC,
     ETH, DAI and KACY built in partnership with our friends back at Yield Yak.`,
     partners: [
       partners.avalanche,
@@ -215,7 +215,7 @@ const tricrypto: ProductDetails  = process.env.NEXT_PUBLIC_MASTER === '1' ?
     fundIcon: tricryptoIcon,
     pid: 4,
     fundLink: 'https://kassandrafoundation.medium.com/kassandras-new-autocompounding-strategy-tricrypto-185eb1fca2c2',
-    fundSummary: `The #K3C represents a yield-earning portfolio composed of BTC,
+    fundSummary: `The K3C represents a yield-earning portfolio composed of BTC,
     ETH, DAI and KACY built in partnership with our friends back at Yield Yak.`,
     partners: [
       partners.avalanche,
