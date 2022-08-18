0112bc1c890a95dfc254879e4781635c67b6b411
Tiago138 <tgnvsctn@gmail.com>
2022-08-18 11:41:05 -0300

    refactor: removed iron session config

--- src/types/iron-session.d.ts
+++
@@ -1,9 +0,0 @@
-import 'iron-session';
-
-declare module 'iron-session' {
-  // eslint-disable-next-line prettier/prettier
-  interface IronSessionData {
-    nonce?: string;
-    address?: string;
-  }
-}
