3391be12f0e25ea0ae7c9227742742e11d65a640
Tiago138 <tgnvsctn@gmail.com>
2022-09-22 15:09:16 -0300

    feat: created fadein componenet

---
+++ src/components/Animations/FadeIn/styles.ts
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+
+interface IFadeInContainerProps {
+  inView: boolean;
+}
+
+export const FadeInContainer = styled.div`
+  opacity: 0;
+
+  transition-duration: 600ms;
+  transition-timing-function: ease-in-out;
+  transition-property: opacity;
+
+  ${({ inView }: IFadeInContainerProps) =>
+    inView &&
+    css`
+      opacity: 1;
+    `}
+`
