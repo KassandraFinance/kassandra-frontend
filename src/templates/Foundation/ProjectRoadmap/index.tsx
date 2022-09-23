7aee43cb633927c661ee9bc0a374bdfa1eba9ac0
Tiago138 <tgnvsctn@gmail.com>
2022-09-23 16:42:36 -0300

    feat: add animaiton on roadmap slider component

--- src/templates/Foundation/ProjectRoadmap/index.tsx
+++ src/templates/Foundation/ProjectRoadmap/index.tsx
@@ -1,10 +1,11 @@
 import RoadMapSlider from '../../../components/RoadMapSlider'
+import FadeIn from '../../../components/Animations/FadeIn'
 
 const ProjectRoadmap = () => {
   return (
-    <>
+    <FadeIn threshold={0.5}>
       <RoadMapSlider />
-    </>
+    </FadeIn>
   )
 }
 
