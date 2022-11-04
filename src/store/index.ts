b524f5bd68a53ed2096b8da13933d48ebe80bf1a
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-03 22:59:35 -0400

    refactor: remove reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import userWalletAddressReducer from './reducers/userWalletAddress'
-import chainIdReducer from './reducers/chainId'
-import poolImagesReducer from './reducers/poolImages'
-import tokenAddress2IndexReducer from './reducers/tokenAddress2Index'
 import daoInfoReducer from './reducers/daoInfoSlice'
 
 export const store = configureStore({
   reducer: {
     userWalletAddress: userWalletAddressReducer,
-    chainId: chainIdReducer,
-    poolImages: poolImagesReducer,
-    tokenAddress2Index: tokenAddress2IndexReducer,
     daoInfo: daoInfoReducer
   }
 })
