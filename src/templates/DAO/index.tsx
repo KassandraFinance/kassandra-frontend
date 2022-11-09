93f5f8ef84840b06243529553ca03adac07e4ab5
tiago <tgnvsctn@gmail.com>
2022-11-09 14:22:57 -0300

    refactor: moved hero to only one component

--- src/templates/DAO/index.tsx
+++ src/templates/DAO/index.tsx
@@ -1,4 +1,7 @@
-import Hero from './Hero'
+import React from 'react'
+import Image from 'next/image'
+
+import Hero from '../../components/Hero'
 import CommunityTenets from './CommunityTenets'
 import FlowingRevenue from './FlowingRevenue'
 import Governance from './Governance'
@@ -6,13 +9,44 @@ import LockVote from './LockVote'
 import Scarcity from './Scarcity'
 import Tokenomics from './Tokenomics'
 import Contribute from '../../components/Contribute'
+import Button from '../../components/Button'
+import ModalBuyKacy from '../../components/Modals/ModalBuyKacy'
 
 import * as S from './styles'
 
 const TokenHolder = () => {
+  const [isOpenModal, setIsOpenModal] = React.useState<boolean>(false)
+
   return (
     <S.Wrapper>
-      <Hero />
+      <S.HeroWrapper>
+        <Hero
+          titleNumber="03"
+          title="dao"
+          titleColor="#e843c4"
+          subTitle="It’s our community, make your voice heard"
+          description="Shape Kassandra into what you believe in. Propose, vote and contribute where its needed. Help the community flourish. Remember: the bigger the pie, the better your slice."
+          backgroundImg="/assets/images/hero-token-holder.png"
+          backgroundImgHight="100vh"
+        >
+          <Button
+            className="button"
+            size="huge"
+            icon={
+              <Image
+                src="/assets/iconGradient/kacy.svg"
+                width={18}
+                height={18}
+              />
+            }
+            backgroundPrimary
+            text="Buy KACY"
+            onClick={() => {
+              setIsOpenModal(true)
+            }}
+          />
+        </Hero>
+      </S.HeroWrapper>
 
       <CommunityTenets />
 
@@ -30,6 +64,8 @@ const TokenHolder = () => {
         title="Find out how you can contribute"
         text="Accumulate $KACY by investing and contributing to Kassandra and earn a stake in all of our protocol fees."
       />
+
+      <ModalBuyKacy modalOpen={isOpenModal} setModalOpen={setIsOpenModal} />
     </S.Wrapper>
   )
 }
