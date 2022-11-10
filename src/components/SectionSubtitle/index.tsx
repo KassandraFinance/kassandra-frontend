c10a88afb59ead901cc4c310f5c5cfe7303a0ec2
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-10 00:04:59 -0400

    feat: create SectionSubtitle component

---
+++ src/components/SectionSubtitle/index.tsx
@@ -0,0 +1,18 @@
+import { HTMLAttributes } from 'react'
+
+import * as S from './styles'
+
+type HeadingType = HTMLAttributes<HTMLHeadingElement>
+
+export type ISectionSubtitleProps = {
+  text: string,
+  as?: React.ElementType<HeadingType>
+} & HeadingType
+
+const SectionSubtitle = ({ text, as, ...props }: ISectionSubtitleProps) => (
+  <S.SectionSubtitle as={as} {...props}>
+    {text}
+  </S.SectionSubtitle>
+)
+
+export default SectionSubtitle
