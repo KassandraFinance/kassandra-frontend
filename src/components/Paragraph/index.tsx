610331d6234ae9782c45b340d98a12bbb41c4554
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-09 22:37:56 -0400

    feat: add new prop in Paragraph component

--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -7,11 +7,13 @@ export type SpanType = HTMLAttributes<HTMLSpanElement>
 
 type ISectionSubtitleProps = {
   text: string,
-  as?: React.ElementType<ParagraphType | SpanType>
+  as?: React.ElementType<ParagraphType | SpanType>,
+  fontWeight?: number
 } & HTMLAttributes<HTMLParagraphElement | HTMLSpanElement>
 
-const Paragraph = ({ text, as, ...props }: ISectionSubtitleProps) => (
-  <S.Paragraph as={as} {...props}>
+// eslint-disable-next-line prettier/prettier
+const Paragraph = ({ text, as, fontWeight=300, ...props }: ISectionSubtitleProps) => (
+  <S.Paragraph as={as} {...props} fontWeight={fontWeight}>
     {text}
   </S.Paragraph>
 )
--- src/components/Paragraph/styles.ts
+++ src/components/Paragraph/styles.ts
@@ -1,9 +1,14 @@
 import styled, { css } from 'styled-components'
 
-export const Paragraph = styled.p`
-  ${({ theme }) => css`
+interface IParagraphProps {
+  fontWeight: number;
+}
+
+// eslint-disable-next-line prettier/prettier
+export const Paragraph = styled.p<IParagraphProps>`
+  ${({ theme, fontWeight }) => css`
     color: ${theme.colors.white};
-    font-weight: ${theme.font.weight.light};
+    font-weight: ${fontWeight};
     font-size: ${theme.font.sizes.font16};
     line-height: ${theme.font.sizes.font24};
 
