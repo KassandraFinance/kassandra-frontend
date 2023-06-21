e97b10a677ff60510a92d96a0d4d2f1d8d7bb398
Tiago <tgnvsctn@gmail.com>
2023-06-21 17:05:16 -0300

    chore: removed react-slick

--- src/components/RoadMapSlider/index.tsx
+++ src/components/RoadMapSlider/index.tsx
@@ -1,60 +1,60 @@
 import React from 'react'
 
-import Slider, { SliderSettings } from '../../components/Slider'
-import RoadMapCard from './RoadMapCard'
+// import Slider, { SliderSettings } from '../../components/Slider'
+// import RoadMapCard from './RoadMapCard'
 import SectionSubtitle from '../SectionSubtitle'
 
-import { icons, colors, SlickArrowLeft, SlickArrowRight } from './assets'
+// import { icons, colors, SlickArrowLeft, SlickArrowRight } from './assets'
 
 import * as S from './styles'
-import useMatomoEcommerce from '../../hooks/useMatomoEcommerce'
+// import useMatomoEcommerce from '../../hooks/useMatomoEcommerce'
 
 const RoadMapSlider = () => {
-  const { trackEventFunction } = useMatomoEcommerce()
-  const settings: SliderSettings = {
-    arrows: true,
-    slidesToShow: 2.3,
-    lazyLoad: 'ondemand',
-    infinite: true,
-    centerMode: true,
-    centerPadding: '30px',
-    className: 'center',
-    responsive: [
-      {
-        breakpoint: 960,
-        settings: {
-          dots: true,
-          arrows: false,
-          slidesToShow: 2.3,
-          centerPadding: '15px'
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: false,
-          slidesToShow: 1,
-          centerMode: false
-        }
-      }
-    ],
-    prevArrow: (
-      <SlickArrowLeft
-        onClick={() => {
-          trackEventFunction('click-on-button', 'view-roadmap', 'roadmap-card')
-        }}
-      />
-    ),
-    nextArrow: (
-      <SlickArrowRight
-        onClick={() => {
-          trackEventFunction('click-on-button', 'view-roadmap', 'roadmap-card')
-        }}
-      />
-    )
-  }
+  // const { trackEventFunction } = useMatomoEcommerce()
+  // const settings: SliderSettings = {
+  //   arrows: true,
+  //   slidesToShow: 2.3,
+  //   lazyLoad: 'ondemand',
+  //   infinite: true,
+  //   centerMode: true,
+  //   centerPadding: '30px',
+  //   className: 'center',
+  //   responsive: [
+  //     {
+  //       breakpoint: 960,
+  //       settings: {
+  //         dots: true,
+  //         arrows: false,
+  //         slidesToShow: 2.3,
+  //         centerPadding: '15px'
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 700,
+  //       settings: {
+  //         dots: true,
+  //         arrows: false,
+  //         infinite: false,
+  //         slidesToShow: 1,
+  //         centerMode: false
+  //       }
+  //     }
+  //   ],
+  //   prevArrow: (
+  //     <SlickArrowLeft
+  //       onClick={() => {
+  //         trackEventFunction('click-on-button', 'view-roadmap', 'roadmap-card')
+  //       }}
+  //     />
+  //   ),
+  //   nextArrow: (
+  //     <SlickArrowRight
+  //       onClick={() => {
+  //         trackEventFunction('click-on-button', 'view-roadmap', 'roadmap-card')
+  //       }}
+  //     />
+  //   )
+  // }
 
   return (
     <S.Wrapper>
@@ -62,7 +62,7 @@ const RoadMapSlider = () => {
         <SectionSubtitle text="Project Roadmap" />
       </S.TitleAndIcon>
       <S.Divider />
-      <Slider settings={settings}>
+      {/* <Slider settings={settings}>
         {arrCard.map(card => (
           <RoadMapCard
             key={card.title}
@@ -73,69 +73,69 @@ const RoadMapSlider = () => {
             icon={card.icon}
           />
         ))}
-      </Slider>
+      </Slider> */}
     </S.Wrapper>
   )
 }
 
-const arrCard = [
-  {
-    date: '2022 - Q3',
-    icon: icons.soon,
-    color: colors.soon,
-    title: '3rd Cycle ',
-    items: [
-      {
-        title: 'Community Products',
-        text: [
-          'Open interface for Kassandra DAO members create their own tokenized funds'
-        ]
-      },
-      {
-        title: 'Multichain',
-        text: ['Time to jump into other blockchains']
-      }
-    ]
-  },
-  {
-    date: '2022 - Q1',
-    title: '1st Cycle ',
-    icon: icons.done,
-    color: colors.done,
-    items: [
-      {
-        title: 'IDO',
-        text: ['Launching and bootstrapping KACY']
-      },
-      {
-        title: 'Avalanche Social Index',
-        text: [
-          'A single asset that automatically invests in the most active communities on Avalanche'
-        ]
-      },
-      {
-        title: 'Stake & Farm',
-        text: ['Earn staking KACY and other assets']
-      }
-    ]
-  },
-  {
-    date: '2022 - Q2',
-    icon: icons.done,
-    color: colors.done,
-    title: '2nd Cycle ',
-    items: [
-      {
-        title: 'Governance & DAO',
-        text: ['Vote and take a share of Kassandra profits']
-      },
-      {
-        title: 'Tricrypto Social Index',
-        text: [
-          'Bitcoin, Ethereum, and stablecoins, magically working towards better returns'
-        ]
-      }
-    ]
-  }
-]
+// const arrCard = [
+//   {
+//     date: '2022 - Q3',
+//     icon: icons.soon,
+//     color: colors.soon,
+//     title: '3rd Cycle ',
+//     items: [
+//       {
+//         title: 'Community Products',
+//         text: [
+//           'Open interface for Kassandra DAO members create their own tokenized funds'
+//         ]
+//       },
+//       {
+//         title: 'Multichain',
+//         text: ['Time to jump into other blockchains']
+//       }
+//     ]
+//   },
+//   {
+//     date: '2022 - Q1',
+//     title: '1st Cycle ',
+//     icon: icons.done,
+//     color: colors.done,
+//     items: [
+//       {
+//         title: 'IDO',
+//         text: ['Launching and bootstrapping KACY']
+//       },
+//       {
+//         title: 'Avalanche Social Index',
+//         text: [
+//           'A single asset that automatically invests in the most active communities on Avalanche'
+//         ]
+//       },
+//       {
+//         title: 'Stake & Farm',
+//         text: ['Earn staking KACY and other assets']
+//       }
+//     ]
+//   },
+//   {
+//     date: '2022 - Q2',
+//     icon: icons.done,
+//     color: colors.done,
+//     title: '2nd Cycle ',
+//     items: [
+//       {
+//         title: 'Governance & DAO',
+//         text: ['Vote and take a share of Kassandra profits']
+//       },
+//       {
+//         title: 'Tricrypto Social Index',
+//         text: [
+//           'Bitcoin, Ethereum, and stablecoins, magically working towards better returns'
+//         ]
+//       }
+//     ]
+//   }
+// ]
 export default RoadMapSlider
--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,18 +1,18 @@
-import SlickSlider, { Settings } from 'react-slick'
+// // import SlickSlider, { Settings } from 'react-slick'
 
-import * as S from './styles'
+// import * as S from './styles'
 
-export type SliderSettings = Settings
+// // export type SliderSettings = Settings
 
-export type SliderProps = {
-  children: React.ReactNode
-  settings: SliderSettings
-}
+// export type SliderProps = {
+//   children: React.ReactNode
+//   // settings: SliderSettings
+// }
 
-const Slider = ({ children, settings }: SliderProps) => (
-  <S.Wrapper>
-    <SlickSlider {...settings}>{children}</SlickSlider>
-  </S.Wrapper>
-)
+// const Slider = ({ children, settings }: SliderProps) => (
+//   <S.Wrapper>
+//     {/* <SlickSlider {...settings}>{children}</SlickSlider> */}
+//   </S.Wrapper>
+// )
 
-export default Slider
+// export default Slider
