966687f6437c574ec38e7d27b87d5138f113c82a
Tiago <tgnvsctn@gmail.com>
2023-06-26 16:59:41 -0300

    refactor: removed redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
-import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { useRouter } from 'next/router'
-import { Provider } from 'react-redux'
-import { SWRConfig } from 'swr'
 import { clarity } from 'react-microsoft-clarity'
 import {
   Hydrate,
@@ -16,8 +13,6 @@ import {
 
 import GlobalStyles from '../styles/global'
 import theme from '../styles/theme'
-
-import { store } from '../store'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -51,62 +46,55 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-      <MatomoProvider value={instance}>
-        <ThemeProvider theme={theme}>
-          <Head>
-            {/* Primary Meta Tags */}
-            <meta
-              name="title"
-              content="Kassandra DAO - Tokenized crypto portfolios"
-            />
+        <MatomoProvider value={instance}>
+          <ThemeProvider theme={theme}>
+            <Head>
+              {/* Primary Meta Tags */}
+              <meta
+                name="title"
+                content="Kassandra DAO - Tokenized crypto portfolios"
+              />
 
-            {/* Open Graph / Facebook */}
-            <meta property="og:type" content="website" />
-            <meta
-              property="og:title"
-              content="Kassandra DAO - Tokenized crypto portfolios"
-            />
-            <meta
-              property="og:image"
-              content="https://kassandra.finance/kacy-og.png"
-            />
-            <meta property="og:site_name" content="Kassandra" />
-            <meta property="og:image:width" content="1012" />
-            <meta property="og:image:height" content="506" />
+              {/* Open Graph / Facebook */}
+              <meta property="og:type" content="website" />
+              <meta
+                property="og:title"
+                content="Kassandra DAO - Tokenized crypto portfolios"
+              />
+              <meta
+                property="og:image"
+                content="https://kassandra.finance/kacy-og.png"
+              />
+              <meta property="og:site_name" content="Kassandra" />
+              <meta property="og:image:width" content="1012" />
+              <meta property="og:image:height" content="506" />
 
-            {/* Twitter */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta
-              property="twitter:title"
-              content="Kassandra DAO - Tokenized crypto portfolios"
-            />
-            <meta
-              property="twitter:image"
-              content="https://kassandra.finance/kacy-og.png"
-            />
-            <meta property="twitter:site" content="@dao_kassandra" />
+              {/* Twitter */}
+              <meta property="twitter:card" content="summary_large_image" />
+              <meta
+                property="twitter:title"
+                content="Kassandra DAO - Tokenized crypto portfolios"
+              />
+              <meta
+                property="twitter:image"
+                content="https://kassandra.finance/kacy-og.png"
+              />
+              <meta property="twitter:site" content="@dao_kassandra" />
 
-            <link rel="icon" href="/favicon.ico" />
-            <link
-              rel="icon"
-              href="https://kassandra.finance/favicon.svg"
-              sizes="any"
-            />
-          </Head>
-          <Toastify />
-          <GlobalStyles />
-          <SWRConfig
-            value={{
-              refreshInterval: 10000,
-              fetcher: url => fetch(url).then(res => res.json())
-            }}
-          >
+              <link rel="icon" href="/favicon.ico" />
+              <link
+                rel="icon"
+                href="https://kassandra.finance/favicon.svg"
+                sizes="any"
+              />
+            </Head>
+            <Toastify />
+            <GlobalStyles />
             <Header />
             <Component {...pageProps} />
-          </SWRConfig>
-          {router.pathname !== '/404' && <Footer />}
-        </ThemeProvider>
-      </MatomoProvider>
+            {router.pathname !== '/404' && <Footer />}
+          </ThemeProvider>
+        </MatomoProvider>
       </Hydrate>
     </QueryClientProvider>
   )
