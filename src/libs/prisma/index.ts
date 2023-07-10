8ab46de4823d4020079b0b7245df94cf1314a220
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-07-10 17:01:06 -0300

    refactor: test api request subscribe

--- src/libs/prisma/index.ts
+++ src/libs/prisma/index.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-// eslint-disable-next-line prettier/prettier
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
-}
+// // eslint-disable-next-line prettier/prettier
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined
+// }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient()
+// const prisma =
+//   global.prisma ||
+//   new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
-export default prisma
+// export default prisma
 
