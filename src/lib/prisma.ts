3721dda1a9225b38629e88142ad59c3de0b90e8c
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-07-10 15:49:36 -0300

    refactor: prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// eslint-disable-next-line prettier/prettier
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
