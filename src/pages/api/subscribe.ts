34738eab526628a477e72ceace0268aad872bbbf
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-07-10 16:45:04 -0300

    refactor: test request subscribe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,4 +1,3 @@
-// import { prisma } from '../../libs/prisma'
 import { PrismaClient } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors'
@@ -10,40 +9,49 @@ const isValidEmail = (email: string): boolean => {
   return regex.test(email)
 }
 
-const URL_COINGECKO = 'https://api.coingecko.com/api/v3'
-
 export default async (request: NextApiRequest, response: NextApiResponse) => {
+  await NextCors(request, response, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: 'https://kassandra.finance',
+    optionsSuccessStatus: 200
+  })
+
+  const method = request.method ?? ''
+
   try {
-    await NextCors(request, response, {
-      // Options
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: 'https://kassandra.finance',
-      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-    })
-    isValidEmail('email@')
     const prisma = new PrismaClient()
-    await prisma.subscribe.create({
-      data: { email: 'g@kassandra.finance' }
-    })
+    if (method === 'POST') {
+      const { email } = request.body as { email: string }
+      if (!email || email.length > 100 || !isValidEmail(email))
+        return response.status(400).json({ message: 'Invalid email' })
 
-    const urlPriceKacyOnCoingecko = `${URL_COINGECKO}/coins/kassandra?localization=false&tickers=false&community_data=false`
+      const emailExists = await prisma.subscribe.findUnique({
+        where: { email: email.toLowerCase() }
+      })
 
-    const responseKacyPrice = await fetch(urlPriceKacyOnCoingecko)
-    const responseKacyPriceJson = await responseKacyPrice.json()
-    const kacy = responseKacyPriceJson.market_data
+      if (emailExists) {
+        return response
+          .status(400)
+          .json({ message: 'Email already subscribed' })
+      }
 
-    response.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate')
+      await prisma.subscribe.create({
+        data: { email: email.toLowerCase() }
+      })
 
-    response.json({
-      kacyPrice: kacy.current_price.usd,
-      supply: kacy.circulating_supply,
-      marketCap: kacy.market_cap.usd,
-      kacyPercentage: kacy.price_change_percentage_24h
-    })
+      return response
+        .status(201)
+        .json({ message: 'The email has been subscribed' })
+    }
+
+    return (
+      response
+        .status(405)
+        // .setHeader('Allow', ['POST'])
+        .json({ message: `Method ${method} Not Allowed` })
+    )
   } catch (error) {
-    response.json({
-      error
-    })
+    return response.status(500).json({ message: 'Internal Server Error' })
   }
 }
 
