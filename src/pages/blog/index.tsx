f722a042fd062079ffb98e5c8cdcf5e7c9a3eba2
Bernardo Muller <bernardo.muller.bm@gmail.com>
2023-07-12 14:47:03 -0300

    fix: Head in the wrong place, should be on the page itself, not the template

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,26 @@
+import Head from 'next/head'
+
 import Blog from '@/templates/Blog'
 
 const Index = () => {
-  return <Blog />
+  return (
+    <>
+      <Head>
+        {/* Primary Meta Tags */}
+        <title>Kassandra Blog</title>
+        <meta name="description" content="It's a me, a Blog(io)" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:url" content="https://kassandra.finance/blog" />
+        <meta property="og:description" content="A Blog" />
+
+        {/* Twitter */}
+        <meta property="twitter:url" content="https://kassandra.finance/blog" />
+        <meta property="twitter:description" content="A Blog" />
+      </Head>
+      <Blog />
+    </>
+  )
 }
 
 export default Index
--- src/templates/Blog/index.tsx
+++ src/templates/Blog/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 import Button from '@/components/Button'
 import Hero from '@/components/Hero'
 import { Tag } from '@/components/Blog/Tag'
@@ -7,20 +5,6 @@ import { Tag } from '@/components/Blog/Tag'
 const Blog = () => {
   return (
     <>
-      <Head>
-        {/* Primary Meta Tags */}
-        <title>Kassandra Blog</title>
-        <meta name="description" content="It's a me, a Blog(io)" />
-
-        {/* Open Graph / Facebook */}
-        <meta property="og:url" content="https://kassandra.finance/blog" />
-        <meta property="og:description" content="A Blog" />
-
-        {/* Twitter */}
-        <meta property="twitter:url" content="https://kassandra.finance/blog" />
-        <meta property="twitter:description" content="A Blog" />
-      </Head>
-
       <Hero
         titleNumber="04"
         title="blog"
