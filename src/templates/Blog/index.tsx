98d3658905a02730b73bcfca4d35f7cda526cda8
Nicholas Costa <nicholascostadev@gmail.com>
2023-07-12 17:17:33 -0300

    feat: add IconButton

--- src/templates/Blog/index.tsx
+++ src/templates/Blog/index.tsx
@@ -1,4 +1,6 @@
 import { Tag } from '@/components/Blog/Tag'
+import IconButton from '@/components/IconButton'
+import { DiscordIcon } from '@/components/Icons/Discord'
 
 const Blog = () => {
   return (
@@ -7,6 +9,7 @@ const Blog = () => {
       <Tag variant="primary" shape="rounded" size="medium">
         Tag
       </Tag>
+      <IconButton icon={<DiscordIcon />} />
     </div>
   )
 }
