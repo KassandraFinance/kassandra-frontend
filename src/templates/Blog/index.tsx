7eefa78a6edea6c23610f5d1ad9624dbc14a6b29
Nicholas Costa <nicholascostadev@gmail.com>
2023-07-12 14:32:36 -0300

    feat: add Tag component

--- src/templates/Blog/index.tsx
+++ src/templates/Blog/index.tsx
@@ -1,7 +1,12 @@
+import { Tag } from '@/components/Blog/Tag'
+
 const Blog = () => {
   return (
     <div>
       <p>Template do blog</p>
+      <Tag variant="primary" shape="rounded" size="medium">
+        Tag
+      </Tag>
     </div>
   )
 }
