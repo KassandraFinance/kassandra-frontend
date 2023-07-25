5fb97bbb17f31e9a855e85b125c2ff1b6c0f13b7
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-07-25 20:44:07 -0300

    feat: questions and answers section

---
+++ src/templates/IncentivesProgram/QuestionsAndAnswers/index.tsx
@@ -0,0 +1,20 @@
+import Subtitle from '@/components/Subtitle'
+import CircleGradient from '@/components/CircleGradient'
+import FAQ from './FAQ'
+
+import * as S from './styles'
+
+const QuestionsAndAnswers = () => {
+  return (
+    <S.QuestionsAndAnswers>
+      <S.QuestionsAndAnswersHeader>
+        <CircleGradient text="?" width={4.7} height={4.4} />
+        <Subtitle text="Questions & Answers" />
+        <S.Hr />
+      </S.QuestionsAndAnswersHeader>
+      <FAQ />
+    </S.QuestionsAndAnswers>
+  )
+}
+
+export default QuestionsAndAnswers
