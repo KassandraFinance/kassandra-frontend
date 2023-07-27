d32818cfbba14af3791388e432e4a1f8dd440c8d
ChristanDaniel <christian_daniel04@hotmail.com>
2023-07-26 21:48:57 -0400

    feat: create SocialMidia component

---
+++ src/components/SocialMidia/styles.ts
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components'
+
+export const SocialMidia = styled.ul`
+  ${({ theme }) => css`
+    display: flex;
+    gap: 1.4rem;
+  `}
+`
+
+export const SocialMidiaContent = styled.a`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3.2rem;
+    width: 3.2rem;
+
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
+    border: 1px solid rgba(255, 255, 255, 0);
+
+    transition: border ${theme.transition.default};
+
+    /* img {
+      padding: 0.5rem;
+    } */
+
+    &:hover {
+      border: 1px solid rgba(255, 255, 255, 0.3);
+    }
+  `}
+`
