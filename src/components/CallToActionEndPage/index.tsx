4072c35c01b25c821ba63d40107604843f4bf651
ChristanDaniel <christian_daniel04@hotmail.com>
2024-01-23 15:08:57 -0400

    feat: create CallToActionEndPage component

---
+++ src/components/CallToActionEndPage/index.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+import NewButton from '../NewButton'
+
+import { ArrowRightCircle } from '@/Icons/Arrow-right-circle'
+
+import * as S from './styles'
+
+type ICallToActionEndPageProps = {
+  text: string
+  buttonText: string
+  href: string
+}
+
+const CallToActionEndPage = ({
+  text,
+  buttonText,
+  href
+}: ICallToActionEndPageProps) => (
+  <S.CallToActionEndPage>
+    <p>{text}</p>
+
+    <Link href={href} passHref>
+      <NewButton
+        as="a"
+        text={buttonText}
+        background="primary"
+        icon={<ArrowRightCircle />}
+      />
+    </Link>
+  </S.CallToActionEndPage>
+)
+
+export default CallToActionEndPage
